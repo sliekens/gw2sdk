68c261e6500a90e8c1b87e0f49530b322e165dc1
Steven Liekens <steven.liekens@gmail.com>
2019-05-18 03:31:50 +0200

    Add some Todos

--- GW2SDK/AssemblyInfo.cs
+++ GW2SDK/AssemblyInfo.cs
@@ -1,3 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
 
+/**
+ * Use this comment to sum up our technical debt, it integrates nicely with To-do explorer
+ *
+ * TODO: add custom list type with ListMetaData
+ *
+ * TODO: add ListMetaData to all existing services
+ *
+ */
 [assembly: InternalsVisibleTo("GW2SDK.Tests")]
