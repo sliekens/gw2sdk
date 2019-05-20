6773abea3994e34d2daecf1383f30e67261179b0
Steven Liekens <steven.liekens@gmail.com>
2019-05-21 00:39:51 +0200

    Make Scope work on methods, make it public

--- GW2SDK/Infrastructure/ScopeAttribute.cs
+++ GW2SDK/Infrastructure/ScopeAttribute.cs
@@ -3,8 +3,8 @@ using GW2SDK.Features.Tokens;
 
 namespace GW2SDK.Infrastructure
 {
-    [AttributeUsage(AttributeTargets.Property)]
-    internal sealed class ScopeAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public sealed class ScopeAttribute : Attribute
     {
         public ScopeAttribute(Permission permission)
         {
