044dcfe6fce5f4ff55c7f21739f66dc49d428d1b
Steven Liekens <steven.liekens@gmail.com>
2019-05-25 01:45:31 +0200

    Invent converter for discriminated json

---
+++ GW2SDK/Infrastructure/DiscriminatedJsonConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GW2SDK.Infrastructure
+{
+    public sealed class DiscriminatedJsonConverter : JsonConverter
+    {
+        private readonly DiscriminatorOptions _discriminatorOptions;
+
+        public DiscriminatedJsonConverter(Type concreteDiscriminatorOptionsType)
+            : this((DiscriminatorOptions) Activator.CreateInstance(concreteDiscriminatorOptionsType))
+        {
+        }
+
+        public DiscriminatedJsonConverter([NotNull] DiscriminatorOptions discriminatorOptions)
+        {
+            _discriminatorOptions = discriminatorOptions ?? throw new ArgumentNullException(nameof(discriminatorOptions));
+        }
+
+        public override bool CanConvert(Type objectType) => _discriminatorOptions.BaseType.IsAssignableFrom(objectType);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
+            var json = JObject.Load(reader);
+            var discriminator = json.Property(_discriminatorOptions.Discriminator);
+            if (!_discriminatorOptions.SerializeDiscriminator)
+            {
+                discriminator.Remove();
+            }
+
+            var jsonTypeName = discriminator.Value.ToString();
+            foreach (var typeInfo in _discriminatorOptions.GetTypes())
+            {
+                var (typeName, type) = typeInfo;
+                if (jsonTypeName == typeName)
+                {
+                    return ReadJsonImpl(json.CreateReader(), type, serializer);
+                }
+            }
+
+            return ReadJsonImpl(json.CreateReader(), _discriminatorOptions.FallbackType, serializer);
+        }
+
+        private object ReadJsonImpl(JsonReader reader, Type objectType, JsonSerializer serializer)
+        {
+            var value = _discriminatorOptions.Create(objectType);
+            if (value == null)
+            {
+                throw new JsonSerializationException("No object created.");
+            }
+
+            serializer.Populate(reader, value);
+            return value;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotSupportedException("DiscriminatedJsonConverter should only be used while deserializing.");
+        }
+    }
+}
---
+++ GW2SDK/Infrastructure/DiscriminatorOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GW2SDK.Infrastructure
+{
+    /// <summary>
+    ///     Extend this class to configure a type with a discriminator field.
+    /// </summary>
+    public abstract class DiscriminatorOptions
+    {
+        /// <summary>Gets the base type, which is typically (but not necessarily) abstract.</summary>
+        public abstract Type BaseType { get; }
+
+        /// <summary>Gets the name of the discriminator field.</summary>
+        public abstract string Discriminator { get; }
+
+        /// <summary>Gets the CLR type that will be used if no suitable type is found.</summary>
+        public abstract Type FallbackType { get; }
+
+        /// <summary>Returns true if the discriminator should be serialized to the CLR type; otherwise false.</summary>
+        public abstract bool SerializeDiscriminator { get; }
+
+        /// <summary>Gets the mappings from discriminator values to CLR types.</summary>
+        public abstract IEnumerable<(string TypeName, Type Type)> GetTypes();
+
+        /// <summary>Creates an object which will then be populated by the serializer.</summary>
+        public abstract object Create(Type objectType);
+    }
+}
