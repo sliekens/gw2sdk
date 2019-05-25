5d825b803264608a59b936f286415062c94174dd
Steven Liekens <steven.liekens@gmail.com>
2019-05-25 23:55:20 +0200

    Amend

--- GW2SDK.Tests/Features/Subtokens/SubtokenServiceTest.cs
+++ GW2SDK.Tests/Features/Subtokens/SubtokenServiceTest.cs
@@ -5,36 +5,22 @@ using GW2SDK.Extensions;
 using GW2SDK.Features.Subtokens;
 using GW2SDK.Infrastructure.Subtokens;
 using GW2SDK.Tests.Shared.Fixtures;
-using Microsoft.Extensions.Http;
-using Polly;
 using Xunit;
 
 namespace GW2SDK.Tests.Features.Subtokens
 {
-    public class SubtokenServiceTest : IClassFixture<ConfigurationFixture>
+    public class SubtokenServiceTest : IClassFixture<HttpFixture>
     {
-        public SubtokenServiceTest(ConfigurationFixture configuration)
+        public SubtokenServiceTest(HttpFixture http)
         {
-            _configuration = configuration;
+            _http = http;
         }
 
-        private readonly ConfigurationFixture _configuration;
+        private readonly HttpFixture _http;
 
         private SubtokenService CreateSut()
         {
-            var policy = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(3));
-            var handler = new PolicyHttpMessageHandler(policy)
-            {
-                InnerHandler = new SocketsHttpHandler()
-            };
-            var http = new HttpClient(handler)
-            {
-                BaseAddress = _configuration.BaseAddress
-            };
-            http.UseLatestSchemaVersion();
-            http.UseAccessToken(_configuration.ApiKeyFull);
-
-            var api = new SubtokenJsonService(http);
+            var api = new SubtokenJsonService(_http.HttpFullAccess);
             return new SubtokenService(api);
         }
 
