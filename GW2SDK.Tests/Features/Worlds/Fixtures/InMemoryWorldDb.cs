0cbc426610edd99b0aaa6ebd8f65b6f65148b969
Steven Liekens <steven.liekens@gmail.com>
2019-05-26 07:40:05 +0200

    Add experimental snapshot mechanism

--- GW2SDK.Tests/Features/Worlds/Fixtures/InMemoryWorldDb.cs
+++ GW2SDK.Tests/Features/Worlds/Fixtures/InMemoryWorldDb.cs
@@ -1,9 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GW2SDK.Tests.Features.Worlds.Fixtures
 {
     public class InMemoryWorldDb
     {
-        public List<string> Worlds { get; } = new List<string>();
+        private readonly List<string> _db = new List<string>();
+
+        public IReadOnlyList<string> Worlds => _db.AsReadOnly();
+
+        public void AddWorld(string json)
+        {
+            _db.Add(json);
+        }
+
+        public IImmutableList<int> GetIds() => (from entry in _db
+            let jobject = JObject.Parse(entry)
+            let id = (JValue) jobject.SelectToken("id")
+            select Convert.ToInt32(id.Value)).ToImmutableList();
+
+        public async Task LoadSnapshot()
+        {
+            using (var snapshot = File.Open("worlds.json", FileMode.OpenOrCreate, FileAccess.Read, FileShare.None))
+            {
+                if (snapshot.Length == 0)
+                {
+                    return;
+                }
+
+                using (var reader = new StreamReader(snapshot, Encoding.UTF8, false, 1024, true))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    jsonReader.SupportMultipleContent = true;
+                    jsonReader.Read();
+                    do
+                    {
+                        var json = await JObject.LoadAsync(jsonReader);
+                        _db.Add(json.ToString(Formatting.None));
+                    } while (jsonReader.Read());
+                }
+            }
+        }
+
+        public async Task CreateSnapshot()
+        {
+            using (var snapshot = File.Open("worlds.json", FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+            using (var sw = new StreamWriter(snapshot, new UTF8Encoding(false)))
+            using (var writer = new JsonTextWriter(sw))
+            {
+                foreach (var entry in _db) await writer.WriteRawAsync(entry);
+            }
+        }
     }
 }
--- GW2SDK.Tests/Features/Worlds/Fixtures/WorldFixture.cs
+++ GW2SDK.Tests/Features/Worlds/Fixtures/WorldFixture.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using GW2SDK.Infrastructure.Worlds;
 using GW2SDK.Tests.Shared.Fixtures;
@@ -14,18 +17,54 @@ namespace GW2SDK.Tests.Features.Worlds.Fixtures
 
         public async Task InitializeAsync()
         {
+            await Db.LoadSnapshot();
+
             using (var http = new HttpFixture())
-            using (var request = new GetAllWorldsRequest())
-            using (var response = await http.Http.SendAsync(request))
-            using (var reader = new StreamReader(await response.Content.ReadAsStreamAsync()))
-            using (var jsonReader = new JsonTextReader(reader))
             {
-                response.EnsureSuccessStatusCode();
-                var array = await JToken.ReadFromAsync(jsonReader);
-                foreach (var world in array.Children<JObject>()) Db.Worlds.Add(world.ToString(Formatting.None));
+                var ids = await GetWorldIds(http.Http);
+
+                ids = ids.Except(Db.GetIds()).ToList();
+
+                while (ids.Count != 0)
+                {
+                    var batch = ids.Take(200).ToList();
+                    foreach (var world in await GetWorldsByIdRaw(http.Http, batch))
+                    {
+                        Db.AddWorld(world);
+                        ids = ids.Except(batch).ToList();
+                    }
+                }
             }
+
+            await Db.CreateSnapshot();
         }
 
         public Task DisposeAsync() => Task.CompletedTask;
+
+        private async Task<List<int>> GetWorldIds(HttpClient http)
+        {
+            using (var request = new GetWorldIdsRequest())
+            using (var response = await http.SendAsync(request))
+            using (var responseReader = new StreamReader(await response.Content.ReadAsStreamAsync()))
+            using (var jsonReader = new JsonTextReader(responseReader))
+            {
+                response.EnsureSuccessStatusCode();
+                var serializer = new JsonSerializer();
+                return serializer.Deserialize<List<int>>(jsonReader);
+            }
+        }
+
+        private async Task<List<string>> GetWorldsByIdRaw(HttpClient http, IReadOnlyList<int> worldIds)
+        {
+            using (var request = new GetWorldsByIdRequest(worldIds))
+            using (var response = await http.SendAsync(request))
+            using (var responseReader = new StreamReader(await response.Content.ReadAsStreamAsync()))
+            using (var jsonReader = new JsonTextReader(responseReader))
+            {
+                response.EnsureSuccessStatusCode();
+                var array = await JToken.ReadFromAsync(jsonReader);
+                return array.Children<JObject>().Select(world => world.ToString(Formatting.None)).ToList();
+            }
+        }
     }
 }
