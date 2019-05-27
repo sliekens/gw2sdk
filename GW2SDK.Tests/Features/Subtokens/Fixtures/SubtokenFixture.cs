39850cc091a87c157482195a4492a03c2fc1a21e
Steven Liekens <steven.liekens@gmail.com>
2019-05-27 18:32:45 +0200

    Add Subtoken test with fixture

---
+++ GW2SDK.Tests/Features/Subtokens/Fixtures/SubtokenFixture.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using GW2SDK.Infrastructure.Subtokens;
+using GW2SDK.Tests.Shared;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Subtokens.Fixtures
+{
+    public class SubtokenFixture : IAsyncLifetime
+    {
+        public string CreatedSubtoken { get; private set; }
+
+        public async Task InitializeAsync()
+        {
+            var http = HttpClientFactory.CreateDefault();
+
+            var requestBuilder = new CreateSubtokenRequestBuilder();
+            requestBuilder.UseAccessToken(ConfigurationManager.Instance.ApiKeyFull);
+
+            using (var request = requestBuilder.Build())
+            using (var response = await http.SendAsync(request).ConfigureAwait(false))
+            {
+                response.EnsureSuccessStatusCode();
+                CreatedSubtoken = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+    }
+}
---
+++ GW2SDK.Tests/Features/Subtokens/SubtokenTest.cs
@@ -0,0 +1,37 @@
+﻿using GW2SDK.Features.Accounts;
+using GW2SDK.Features.Subtokens;
+using GW2SDK.Infrastructure;
+using GW2SDK.Tests.Features.Subtokens.Fixtures;
+using GW2SDK.Tests.Shared;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GW2SDK.Tests.Features.Subtokens
+{
+    public class SubtokenTest : IClassFixture<SubtokenFixture>
+    {
+        public SubtokenTest(SubtokenFixture fixture, ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            _output = output;
+        }
+
+        private readonly SubtokenFixture _fixture;
+        private readonly ITestOutputHelper _output;
+
+        [Fact]
+        [Trait("Feature",    "Subtokens")]
+        [Trait("Category",   "Integration")]
+        [Trait("Importance", "Critical")]
+        public void Subtoken_ShouldHaveNoMissingMembers()
+        {
+            var settings = new JsonSerializerSettingsBuilder().UseMissingMemberHandling(MissingMemberHandling.Error)
+                                                              .UseTraceWriter(new XunitTraceWriter(_output))
+                                                              .Build();
+
+            // Next statement throws if there are missing members
+            _ = JsonConvert.DeserializeObject<CreatedSubtoken>(_fixture.CreatedSubtoken, settings);
+        }
+    }
+}
