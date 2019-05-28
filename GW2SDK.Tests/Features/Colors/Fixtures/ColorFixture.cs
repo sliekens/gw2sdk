9adbdb7a16f90438fe4102c0e245f150cd75f102
Steven Liekens <steven.liekens@gmail.com>
2019-05-28 21:27:24 +0200

    Refactor ColorFixture

--- GW2SDK.Tests/Features/Colors/Fixtures/ColorFixture.cs
+++ GW2SDK.Tests/Features/Colors/Fixtures/ColorFixture.cs
@@ -19,44 +19,25 @@ namespace GW2SDK.Tests.Features.Colors.Fixtures
         {
             var http = HttpClientFactory.CreateDefault();
 
-            var ids = await GetColorIds(http);
-
-            ids = ids.Except(Db.Index).ToList();
-
-            while (ids.Count != 0)
+            // Seed InMemoryColorDb with API data for later use in integration tests
+            foreach (var color in await GetAllColorsRaw(http))
             {
-                var batch = ids.Take(200).ToList();
-                foreach (var color in await GetColorsByIdRaw(http, batch))
-                {
-                    Db.AddColor(color);
-                    ids = ids.Except(batch).ToList();
-                }
+                Db.AddColor(color);
             }
         }
 
         public Task DisposeAsync() => Task.CompletedTask;
 
-        private async Task<List<int>> GetColorIds(HttpClient http)
+        private async Task<List<string>> GetAllColorsRaw(HttpClient http)
         {
-            using (var request = new GetColorIdsRequest())
+            using (var request = new GetAllColorsRequest())
             using (var response = await http.SendAsync(request))
             using (var responseReader = new StreamReader(await response.Content.ReadAsStreamAsync()))
             using (var jsonReader = new JsonTextReader(responseReader))
             {
                 response.EnsureSuccessStatusCode();
-                var serializer = new JsonSerializer();
-                return serializer.Deserialize<List<int>>(jsonReader);
-            }
-        }
 
-        private async Task<List<string>> GetColorsByIdRaw(HttpClient http, IReadOnlyList<int> colorIds)
-        {
-            using (var request = new GetColorsByIdRequest.Builder(colorIds).GetRequest())
-            using (var response = await http.SendAsync(request))
-            using (var responseReader = new StreamReader(await response.Content.ReadAsStreamAsync()))
-            using (var jsonReader = new JsonTextReader(responseReader))
-            {
-                response.EnsureSuccessStatusCode();
+                // API returns a JSON array but we want a List of JSON objects instead
                 var array = await JToken.ReadFromAsync(jsonReader);
                 return array.Children<JObject>().Select(obj => obj.ToString(Formatting.None)).ToList();
             }
