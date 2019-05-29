0a3a93704647cc401417ec9f49e226013caeef54
Steven Liekens <steven.liekens@gmail.com>
2019-05-30 01:28:36 +0200

    Amend

--- GW2SDK.Tests/Features/Subtokens/CreateSubtokenRequestTest.cs
+++ GW2SDK.Tests/Features/Subtokens/CreateSubtokenRequestTest.cs
@@ -8,16 +8,6 @@ namespace GW2SDK.Tests.Features.Subtokens
         [Fact]
         [Trait("Feature",  "Subtokens")]
         [Trait("Category", "Unit")]
-        public void CreateSubtokenRequest_WithDefaultAccessToken_ShouldHaveNoAuthorizationHeader()
-        {
-            var sut = new CreateSubtokenRequest.Builder().GetRequest();
-
-            Assert.Null(sut.Headers.Authorization);
-        }
-
-        [Fact]
-        [Trait("Feature",  "Subtokens")]
-        [Trait("Category", "Unit")]
         public void CreateSubtokenRequest_WithAccessTokenNull_ShouldHaveNoAuthorizationHeader()
         {
             var sut = new CreateSubtokenRequest.Builder().GetRequest();
