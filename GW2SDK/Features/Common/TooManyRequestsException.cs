bb24e97b658e2e337b5e37915b4c234d49cd25da
Steven Liekens <steven.liekens@gmail.com>
2019-06-15 01:50:25 +0200

    Handle 429 responses in a message handler

---
+++ GW2SDK/Features/Common/TooManyRequestsException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GW2SDK.Infrastructure;
+
+namespace GW2SDK.Features.Common
+{
+    public sealed class TooManyRequestsException : InvalidOperationException
+    {
+        public TooManyRequestsException()
+        {
+        }
+
+        public TooManyRequestsException([CanBeNull] string message)
+            : base(message)
+        {
+        }
+
+        public TooManyRequestsException([CanBeNull] string message, [CanBeNull] Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
