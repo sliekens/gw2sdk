328e985d40eab4bbed5312e0e7bdcbf370a7b0b3
Steven Liekens <steven.liekens@gmail.com>
2019-06-15 01:50:25 +0200

    Handle 429 responses in a message handler

---
+++ GW2SDK/Infrastructure/Common/HttpStatusCodeEx.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace GW2SDK.Infrastructure.Common
+{
+    public static class HttpStatusCodeEx
+    {
+        public const HttpStatusCode TooManyRequests = (HttpStatusCode) 429;
+    }
+}
