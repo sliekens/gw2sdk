31b29972281df6fdf62558ec1110b12070b2bd66
Steven Liekens <steven.liekens@gmail.com>
2019-06-15 02:10:59 +0200

    Amend

---
+++ GW2SDK/Infrastructure/Common/RateLimitHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GW2SDK.Features.Common;
+using Newtonsoft.Json.Linq;
+
+namespace GW2SDK.Infrastructure.Common
+{
+    public sealed class RateLimitHandler : DelegatingHandler
+    {
+        public RateLimitHandler()
+        {
+        }
+
+        public RateLimitHandler([NotNull] HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            if (response.StatusCode == HttpStatusCodeEx.TooManyRequests)
+            {
+                var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var text = JObject.Parse(json)["error"].ToString();
+                throw new TooManyRequestsException(text);
+            }
+
+            return response;
+        }
+    }
+}
