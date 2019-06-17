22d55a825a16d1dd82ee8e428d17bc015eb08ab5
Steven Liekens <steven.liekens@gmail.com>
2019-06-18 00:41:41 +0200

    Implement GetAchievementCategories

---
+++ GW2SDK.Tests/Features/Achievements.Categories/AchievementCategoryServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using GW2SDK.Features.Achievements.Categories;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Achievements.Categories
+{
+    public class AchievementCategoryServiceTest
+    {
+        [Fact]
+        [Trait("Feature",  "Achievements.Categories")]
+        [Trait("Category", "Integration")]
+        public async Task GetAchievementCategories_ShouldReturnAllAchievementCategories()
+        {
+            var services = new Container();
+            var sut = services.Resolve<AchievementCategoryService>();
+
+            var actual = await sut.GetAchievementCategories();
+
+            Assert.Equal(actual.ResultTotal, actual.Count);
+        }
+    }
+}
