0855d30770a2001108d8d736e15214e693eceefe
Steven Liekens <steven.liekens@gmail.com>
2019-06-17 22:33:10 +0200

    Add test data for skins

---
+++ GW2SDK.Tests/Features/Skins/GetSkinsByIdsRequestBuilderTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using GW2SDK.Infrastructure.Skins;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Skins
+{
+    public class GetSkinsByIdsRequestBuilderTest
+    {
+        [Fact]
+        [Trait("Feature",  "Skins")]
+        [Trait("Category", "Unit")]
+        public void Constructor_WithIdsNull_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>("skinIds",
+                () =>
+                {
+                    _ = new GetSkinsByIdsRequest.Builder(null);
+                });
+        }
+
+        [Fact]
+        [Trait("Feature",  "Skins")]
+        [Trait("Category", "Unit")]
+        public void Constructor_WithIdsEmpty_ShouldThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>("skinIds",
+                () =>
+                {
+                    _ = new GetSkinsByIdsRequest.Builder(new int[0]);
+                });
+        }
+
+        [Fact]
+        [Trait("Feature",  "Skins")]
+        [Trait("Category", "Unit")]
+        public void GetRequest_MethodShouldBeGet()
+        {
+            var ids = new List<int> { 1, 2, 3 };
+
+            var sut = new GetSkinsByIdsRequest.Builder(ids);
+
+            var actual = sut.GetRequest();
+
+            Assert.Equal(HttpMethod.Get, actual.Method);
+        }
+
+        [Fact]
+        [Trait("Feature",  "Skins")]
+        [Trait("Category", "Unit")]
+        public void GetRequest_ShouldSerializeIdsAsQueryString()
+        {
+            var ids = new List<int> { 1, 2, 3 };
+
+            var sut = new GetSkinsByIdsRequest.Builder(ids);
+
+            var actual = sut.GetRequest();
+
+            var expected = new Uri("/v2/skins?ids=1,2,3", UriKind.Relative);
+
+            Assert.Equal(expected, actual.RequestUri);
+        }
+    }
+}
---
+++ Invoke-GetJsonSkins.ps1
@@ -0,0 +1,1 @@
+dotnet run -p .\GW2SDK.TestDataHelper\ --feature:skins --out-file:"$PSScriptRoot\GW2SDK.Tests\Data\skins.json"
