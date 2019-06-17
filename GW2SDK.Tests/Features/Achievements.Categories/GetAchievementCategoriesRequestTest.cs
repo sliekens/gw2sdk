122efb648c9d45abd437edcf068af09213e52a92
Steven Liekens <steven.liekens@gmail.com>
2019-06-17 22:56:18 +0200

    Add test data for achievement categories

---
+++ GW2SDK.Tests/Features/Achievements.Categories/GetAchievementCategoriesRequestTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using GW2SDK.Infrastructure.Achievements.Categories;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Achievements.Categories
+{
+    public class GetAchievementCategoriesRequestTest
+    {
+        [Fact]
+        [Trait("Feature",  "Colors")]
+        [Trait("Category", "Unit")]
+        public void Method_ShouldBeGet()
+        {
+            var sut = new GetAchievementCategoriesRequest();
+
+            Assert.Equal(HttpMethod.Get, sut.Method);
+        }
+
+        [Fact]
+        [Trait("Feature",  "Colors")]
+        [Trait("Category", "Unit")]
+        public void RequestUri_ShouldBeV2AchievementsCategoriesBulk()
+        {
+            var sut = new GetAchievementCategoriesRequest();
+
+            var expected = new Uri("/v2/achievements/categories?ids=all", UriKind.Relative);
+
+            Assert.Equal(expected, sut.RequestUri);
+        }
+    }
+}
