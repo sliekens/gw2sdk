dd7513d392df48f838f6d969a2705a8b5a8b2f85
Steven Liekens <steven.liekens@gmail.com>
2019-06-18 00:05:34 +0200

    Add test data for achievement groups

---
+++ GW2SDK/Infrastructure/Achievements.Groups/GetAchievementGroupsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GW2SDK.Infrastructure.Achievements.Groups
+{
+    public sealed class GetAchievementGroupsRequest : HttpRequestMessage
+    {
+        public GetAchievementGroupsRequest()
+            : base(HttpMethod.Get, new Uri("/v2/achievements/groups?ids=all", UriKind.Relative))
+        {
+        }
+    }
+}
