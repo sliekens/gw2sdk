087b6a90bcc0784b4c6ca0b1792282d3845b284f
Steven Liekens <steven.liekens@gmail.com>
2019-06-28 20:30:11 +0200

    Use an enum for dye slot materials

---
+++ GW2SDK/Features/Skins/Material.cs
@@ -0,0 +1,15 @@
+﻿using GW2SDK.Infrastructure;
+
+namespace GW2SDK.Features.Skins
+{
+    [PublicAPI]
+    public enum Material
+    {
+        // Skip index 0 so that Enum.IsDefined returns false for default(Material)
+        Cloth = 1,
+
+        Leather,
+
+        Metal
+    }
+}
