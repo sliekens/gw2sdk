7188c5bea5202d89b540cd1e3a701f363fde7b06
Steven Liekens <steven.liekens@gmail.com>
2019-09-16 00:19:18 +0200

    Refactor token info DTOs and tests

--- GW2SDK/Features/Tokens/SubtokenInfo.cs
+++ GW2SDK/Features/Tokens/SubtokenInfo.cs
@@ -1,15 +1,21 @@
 using System;
 using GW2SDK.Annotations;
+using Newtonsoft.Json;
 
 namespace GW2SDK.Tokens
 {
     [PublicAPI]
     public sealed class SubtokenInfo : TokenInfo
     {
+        [JsonProperty(Required = Required.Always)]
         public DateTimeOffset ExpiresAt { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public DateTimeOffset IssuedAt { get; set; }
 
+        [CanBeNull]
+        [ItemNotNull]
+        [JsonProperty(Required = Required.DisallowNull)]
         public Uri[] Urls { get; set; }
     }
 }
