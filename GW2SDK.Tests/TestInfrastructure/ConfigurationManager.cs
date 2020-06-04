7ee9881b40615296019a86c4d97913ee311de42a
Steven Liekens <steven.liekens@gmail.com>
2020-06-05 01:10:14 +0200

    Throw when api key is not found

--- GW2SDK.Tests/TestInfrastructure/ConfigurationManager.cs
+++ GW2SDK.Tests/TestInfrastructure/ConfigurationManager.cs
@@ -7,14 +7,18 @@ namespace GW2SDK.Tests.TestInfrastructure
     {
         public ConfigurationManager()
         {
-            Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets<ConfigurationManager>().AddEnvironmentVariables().Build();
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<ConfigurationManager>()
+                .AddEnvironmentVariables()
+                .Build();
         }
 
         public IConfigurationRoot Configuration { get; }
 
-        public string ApiKeyBasic => Configuration["ApiKeyBasic"];
+        public string ApiKeyBasic => Configuration["ApiKeyBasic"] ?? throw new InvalidOperationException("Missing ApiKeyBasic.");
 
-        public string ApiKeyFull => Configuration["ApiKeyFull"];
+        public string ApiKeyFull => Configuration["ApiKeyFull"] ?? throw new InvalidOperationException("Missing ApiKeyFull.");
 
         public Uri BaseAddress => new Uri(Configuration["Authority"], UriKind.Absolute);
 
