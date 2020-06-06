3822c45edffc5b3424f3d2759e8936f95119e8a0
Steven Liekens <steven.liekens@gmail.com>
2020-06-06 12:16:19 +0200

    Use offline data for build test

--- GW2SDK.Tests/Features/Builds/Fixtures/BuildFixture.cs
+++ GW2SDK.Tests/Features/Builds/Fixtures/BuildFixture.cs
@@ -1,26 +1,16 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
-using GW2SDK.Builds.Impl;
+﻿using System.Linq;
 using GW2SDK.Tests.TestInfrastructure;
-using Xunit;
 
 namespace GW2SDK.Tests.Features.Builds.Fixtures
 {
-    public class BuildFixture : IAsyncLifetime
+    public class BuildFixture
     {
-        public string Build { get; private set; }
-
-        public async Task InitializeAsync()
+        public BuildFixture()
         {
-            await using var container = new Container();
-            var http = container.Resolve<IHttpClientFactory>().CreateClient("GW2SDK");
-
-            var request = new BuildRequest();
-            using var response = await http.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            Build = await response.Content.ReadAsStringAsync();
+            var reader = new JsonFlatFileReader();
+            Build = reader.Read("Data/build.json").Single();
         }
 
-        public Task DisposeAsync() => Task.CompletedTask;
+        public string Build { get; }
     }
 }
