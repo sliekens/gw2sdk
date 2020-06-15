ef86acd13bd19fe25116eff5b0399d8c076b2495
Steven Liekens <steven.liekens@gmail.com>
2020-06-16 00:20:08 +0200

    Implement daily achievement service

---
+++ GW2SDK.Tests/Features/Achievements.Dailies/Fixtures/DailyAchievementFixture.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using GW2SDK.Achievements.Dailies.Impl;
+using GW2SDK.Tests.TestInfrastructure;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Achievements.Dailies.Fixtures
+{
+    public class DailyAchievementFixture : IAsyncLifetime
+    {
+        public string DailyAchievements { get; set; }
+
+        public async Task InitializeAsync()
+        {
+            await using var container = new Container();
+            var http = container.Resolve<IHttpClientFactory>().CreateClient("GW2SDK");
+            var request = new DailyAchievementsRequest();
+            using var response = await http.SendAsync(request);
+            DailyAchievements = await response.Content.ReadAsStringAsync();
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+    }
+}
---
+++ GW2SDK/Features/Achievements.Dailies/Impl/DailyAchievementsRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using static System.Net.Http.HttpMethod;
+
+namespace GW2SDK.Achievements.Dailies.Impl
+{
+    public sealed class DailyAchievementsRequest
+    {
+        public static implicit operator HttpRequestMessage(DailyAchievementsRequest _)
+        {
+            var location = new Uri("/v2/achievements/daily", UriKind.Relative);
+            return new HttpRequestMessage(Get, location);
+        }
+    }
+}
