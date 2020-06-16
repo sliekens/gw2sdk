2dc1f65ad5942fd498cb541c3c4adc800c54bb4c
Steven Liekens <steven.liekens@gmail.com>
2020-06-16 18:29:12 +0200

    Add support for tomorrow's dailies

--- GW2SDK.Tests/Features/Achievements.Dailies/Fixtures/DailyAchievementFixture.cs
+++ GW2SDK.Tests/Features/Achievements.Dailies/Fixtures/DailyAchievementFixture.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using GW2SDK.Achievements.Dailies;
 using GW2SDK.Achievements.Dailies.Impl;
 using GW2SDK.Tests.TestInfrastructure;
 using Xunit;
@@ -8,15 +9,20 @@ namespace GW2SDK.Tests.Features.Achievements.Dailies.Fixtures
 {
     public class DailyAchievementFixture : IAsyncLifetime
     {
-        public string DailyAchievements { get; set; }
+        public string Today { get; private set; }
+
+        public string Tomorrow { get; private set; }
 
         public async Task InitializeAsync()
         {
             await using var container = new Container();
             var http = container.Resolve<IHttpClientFactory>().CreateClient("GW2SDK");
-            var request = new DailyAchievementsRequest();
-            using var response = await http.SendAsync(request);
-            DailyAchievements = await response.Content.ReadAsStringAsync();
+
+            using var today = await http.SendAsync(new DailyAchievementsRequest());
+            Today = await today.Content.ReadAsStringAsync();
+
+            using var tomorrow = await http.SendAsync(new DailyAchievementsRequest(Day.Tomorrow));
+            Tomorrow = await tomorrow.Content.ReadAsStringAsync();
         }
 
         public Task DisposeAsync() => Task.CompletedTask;
