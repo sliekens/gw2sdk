c2f7aabe4175c6e8df2ca453b6aba24add9fe81d
Steven Liekens <steven.liekens@gmail.com>
2020-06-26 23:29:38 +0200

    Add Http tests

---
+++ GW2SDK.Tests/Http/StubHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GW2SDK.Tests.Http
+{
+    internal class StubHttpMessageHandler : HttpMessageHandler
+    {
+        internal StubHttpMessageHandler(HttpStatusCode code, string content)
+        {
+            Code = code;
+            Content = content;
+        }
+
+        internal HttpStatusCode Code { get; }
+
+        internal string Content { get; }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+            Task.FromResult(new HttpResponseMessage(Code) { Content = new StringContent(Content) });
+    }
+}
