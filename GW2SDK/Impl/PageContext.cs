f4e744b5e27495d7782d306d2ed4d797888b8f3a
Steven Liekens <steven.liekens@gmail.com>
2020-06-29 21:08:48 +0200

    Add support for link headers

--- GW2SDK/Impl/PageContext.cs
+++ GW2SDK/Impl/PageContext.cs
@@ -2,12 +2,26 @@
 {
     internal sealed class PageContext : IPageContext
     {
-        internal PageContext(int resultTotal, int resultCount, int pageTotal, int pageSize)
+        internal PageContext(
+            int resultTotal,
+            int resultCount,
+            int pageTotal,
+            int pageSize,
+            ContinuationToken first,
+            ContinuationToken self,
+            ContinuationToken last,
+            ContinuationToken? previous,
+            ContinuationToken? next)
         {
             ResultTotal = resultTotal;
             ResultCount = resultCount;
             PageTotal = pageTotal;
             PageSize = pageSize;
+            First = first;
+            Self = self;
+            Last = last;
+            Previous = previous;
+            Next = next;
         }
 
         public int ResultTotal { get; }
@@ -17,5 +31,15 @@
         public int PageTotal { get; }
 
         public int PageSize { get; }
+
+        public ContinuationToken? Previous { get; }
+
+        public ContinuationToken? Next { get; }
+
+        public ContinuationToken First { get; }
+
+        public ContinuationToken Self { get; }
+
+        public ContinuationToken Last { get; }
     }
 }
