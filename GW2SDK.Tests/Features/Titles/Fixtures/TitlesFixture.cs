ff9235aa9382f733f58c8c4aa09ccaacba8d417c
Steven Liekens <steven.liekens@gmail.com>
2020-07-05 22:52:53 +0200

    Add titles service

---
+++ GW2SDK.Tests/Features/Titles/Fixtures/TitlesFixture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GW2SDK.Tests.TestInfrastructure;
+
+namespace GW2SDK.Tests.Features.Titles.Fixtures
+{
+    public class TitlesFixture
+    {
+        public TitlesFixture()
+        {
+            var reader = new JsonFlatFileReader();
+            Titles = reader.Read("Data/titles.json").ToList().AsReadOnly();
+        }
+
+        public IReadOnlyCollection<string> Titles { get; }
+    }
+}
