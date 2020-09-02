a4fec17c159b45249f02513e30cac2c903f87716
Steven Liekens <steven.liekens@gmail.com>
2020-09-02 22:16:20 +0200

    wip

--- GW2SDK/Features/Traits/Impl/TraitJsonReader.cs
+++ GW2SDK/Features/Traits/Impl/TraitJsonReader.cs
@@ -9,13 +9,11 @@ namespace GW2SDK.Traits.Impl
 {
     internal sealed class TraitJsonReader : JsonObjectReader<Trait>
     {
-        private readonly TraitFactDiscriminatingJsonReader _traitFactDiscriminatingJsonReader;
         private readonly UnexpectedPropertyBehavior _unexpectedPropertyBehavior;
 
         private TraitJsonReader(UnexpectedPropertyBehavior unexpectedPropertyBehavior)
         {
             _unexpectedPropertyBehavior = unexpectedPropertyBehavior;
-            _traitFactDiscriminatingJsonReader = new TraitFactDiscriminatingJsonReader(_unexpectedPropertyBehavior);
             Configure(MapTrait);
         }
 
@@ -29,9 +27,8 @@ namespace GW2SDK.Traits.Impl
             trait.Map("order", to => to.Order);
             trait.Map("name", to => to.Name);
             trait.Map("slot", to => to.Slot, (in JsonElement element, in JsonPath path) => Enum.Parse<TraitSlot>(element.GetString(), false));
-            trait.Map("facts", to => to.Facts,
-                (in JsonElement element, in JsonPath path) => _traitFactDiscriminatingJsonReader.Read(element, path), Optional);
-            trait.Map("traited_facts", to => to.TraitedFacts, (in JsonElement element, in JsonPath path) => _traitFactDiscriminatingJsonReader.Read(element, path), Optional);
+            trait.Map("facts", to => to.Facts, MapTraitFact, Optional);
+            trait.Map("traited_facts", to => to.TraitedFacts, MapTraitFact, Optional);
             trait.Map("specialization", to => to.SpezializationId);
             trait.Map("icon", to => to.Icon);
             trait.Map("description", to => to.Description, Optional);
@@ -43,8 +40,8 @@ namespace GW2SDK.Traits.Impl
             traitSkill.UnexpectedPropertyBehavior = _unexpectedPropertyBehavior;
             traitSkill.Map("id", to => to.Id);
             traitSkill.Map("name", to => to.Name);
-            traitSkill.Map("facts", to => to.Facts, (in JsonElement element, in JsonPath path) => _traitFactDiscriminatingJsonReader.Read(element, path));
-            traitSkill.Map("traited_facts", to => to.TraitedFacts, (in JsonElement element, in JsonPath path) => _traitFactDiscriminatingJsonReader.Read(element, path), Optional);
+            traitSkill.Map("facts", to => to.Facts, MapTraitFact);
+            traitSkill.Map("traited_facts", to => to.TraitedFacts, MapTraitFact, Optional);
             traitSkill.Map("description", to => to.Description);
             traitSkill.Map("icon", to => to.Icon);
             traitSkill.Map(
@@ -60,6 +57,41 @@ namespace GW2SDK.Traits.Impl
                 Optional
             );
             traitSkill.Map("chat_link", to => to.ChatLink);
+        }
+
+        private TraitFact MapTraitFact(in JsonElement element, in JsonPath path)
+        {
+            if (element.ValueKind != JsonValueKind.Object)
+            {
+                throw new JsonException("Value is not an object.", path.ToString(), null, null);
+            }
+
+            var discriminator = element.TryGetProperty("type", out var type) ? type.GetString() : "";
+            return discriminator switch
+            {
+                "AttributeAdjust" => AttributeAdjustTraitFactJsonReader.Instance.Read(element, path),
+                "Buff" => BuffTraitFactJsonReader.Instance.Read(element, path),
+                "BuffConversion" => BuffConversionTraitFactJsonReader.Instance.Read(element, path),
+                "ComboField" => ComboFieldTraitFactJsonReader.Instance.Read(element, path),
+                "ComboFinisher" => ComboFinisherTraitFactJsonReader.Instance.Read(element, path),
+                "Damage" => DamageTraitFactJsonReader.Instance.Read(element, path),
+                "Distance" => DistanceTraitFactJsonReader.Instance.Read(element, path),
+                "NoData" => NoDataTraitFactJsonReader.Instance.Read(element, path),
+                "Number" => NumberTraitFactJsonReader.Instance.Read(element, path),
+                "Percent" => PercentTraitFactJsonReader.Instance.Read(element, path),
+                "PrefixedBuff" => PrefixedBuffTraitFactJsonReader.Instance.Read(element, path),
+                "Radius" => RadiusTraitFactJsonReader.Instance.Read(element, path),
+                "Range" => RangeTraitFactJsonReader.Instance.Read(element, path),
+                "Recharge" => RechargeTraitFactJsonReader.Instance.Read(element, path),
+                "StunBreak" => StunBreakTraitFactJsonReader.Instance.Read(element, path),
+                "Time" => TimeTraitFactJsonReader.Instance.Read(element, path),
+                "Unblockable" => UnblockableTraitFactJsonReader.Instance.Read(element, path),
+                // BUG: Life Force Cost trait facts don't have a 'type'
+                _ when element.TryGetProperty("percent", out _) => LifeForceCostTraitFactJsonReader.Instance.Read(element, path),
+                _ when _unexpectedPropertyBehavior == UnexpectedPropertyBehavior.Ignore => TraitFactJsonReader<TraitFact>.Instance.Read(element, path),
+                _ => throw new JsonException($"Could not find a type derived from 'TraitFact' for value '{discriminator}'.", path.ToString(), null, null)
+            };
+
         }
     }
 }
