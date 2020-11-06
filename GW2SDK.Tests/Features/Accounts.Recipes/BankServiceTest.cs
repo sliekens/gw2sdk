5d76991b2df2bc0ac0e845ef579a8ee35fec587b
Steven Liekens <steven.liekens@gmail.com>
2020-11-06 21:10:20 +0100

    Add account recipes service

---
+++ GW2SDK.Tests/Features/Accounts.Recipes/BankServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using GW2SDK.Accounts.Banks;
+using GW2SDK.Accounts.Recipes;
+using GW2SDK.Exceptions;
+using GW2SDK.Tests.TestInfrastructure;
+using Xunit;
+
+namespace GW2SDK.Tests.Features.Accounts.Recipes
+{
+    public class AccountRecipeServiceTest
+    {
+        [Fact]
+        public async Task It_can_get_the_unlocked_recipes()
+        {
+            await using var services = new Container();
+            var sut = services.Resolve<AccountRecipesService>();
+
+            var actual = await sut.GetUnlockedRecipes(ConfigurationManager.Instance.ApiKeyFull);
+
+            Assert.NotEmpty(actual);
+            Assert.All(actual, id => Assert.NotEqual(0, id));
+        }
+
+        [Fact]
+        public async Task It_cant_get_the_recipes_without_an_access_token()
+        {
+            await using var services = new Container();
+            var sut = services.Resolve<AccountRecipesService>();
+
+            var actual = await Record.ExceptionAsync(async() =>
+            {
+                var _ = await sut.GetUnlockedRecipes(accessToken: null);
+            });
+
+            var reason = Assert.IsType<UnauthorizedOperationException>(actual);
+
+            Assert.Equal("Invalid access token", reason.Message);
+        }
+
+        [Fact]
+        public async Task It_cant_get_the_recipes_without_the_unlock_scope()
+        {
+            await using var services = new Container();
+            var sut = services.Resolve<AccountRecipesService>();
+
+            var actual = await Record.ExceptionAsync(async() =>
+            {
+                var _ = await sut.GetUnlockedRecipes(ConfigurationManager.Instance.ApiKeyBasic);
+            });
+
+            var reason = Assert.IsType<UnauthorizedOperationException>(actual);
+
+            Assert.Equal("requires scope unlocks", reason.Message);
+        }
+    }
+}
---
+++ GW2SDK/Features/Accounts.Recipes/Impl/UnlockedRecipesRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using static System.Net.Http.HttpMethod;
+
+namespace GW2SDK.Accounts.Recipes.Impl
+{
+    public sealed class UnlockedRecipesRequest
+    {
+        public UnlockedRecipesRequest(string? accessToken = null)
+        {
+            AccessToken = accessToken;
+        }
+
+        public string? AccessToken { get; }
+
+        public static implicit operator HttpRequestMessage(UnlockedRecipesRequest r)
+        {
+            var location = new Uri("/v2/account/recipes", UriKind.Relative);
+            return new HttpRequestMessage(Get, location)
+            {
+                Headers =
+                {
+                    Authorization = string.IsNullOrWhiteSpace(r.AccessToken)
+                        ? default
+                        : new AuthenticationHeaderValue("Bearer", r.AccessToken)
+                }
+            };
+        }
+    }
+}
