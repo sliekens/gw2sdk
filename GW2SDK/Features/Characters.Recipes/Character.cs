df3a8423b156351af75725bedd05363e6105f138
Steven Liekens <steven.liekens@gmail.com>
2020-11-06 22:21:51 +0100

    Add character recipes service

---
+++ GW2SDK/Features/Characters.Recipes/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GW2SDK.Annotations;
+using Newtonsoft.Json;
+
+namespace GW2SDK.Characters.Recipes
+{
+    [PublicAPI]
+    [DataTransferObject]
+    public sealed class Character
+    {
+        [JsonProperty(Required = Required.Always)]
+        public IEnumerable<int> Recipes { get; set; } = new List<int>(0);
+    }
+}
---
+++ GW2SDK/Features/Characters.Recipes/CharacterRecipesService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GW2SDK.Annotations;
+using GW2SDK.Characters.Recipes.Impl;
+using GW2SDK.Enums;
+using GW2SDK.Impl.JsonConverters;
+using Newtonsoft.Json;
+
+namespace GW2SDK.Characters.Recipes
+{
+    [PublicAPI]
+    public sealed class CharacterRecipesService
+    {
+        private readonly HttpClient _http;
+
+        public CharacterRecipesService(HttpClient http)
+        {
+            _http = http ?? throw new ArgumentNullException(nameof(http));
+        }
+
+        [Scope(Permission.Characters, Permission.Inventories)]
+        public async Task<Character?> GetUnlockedRecipes(string characterId, string? accessToken = null)
+        {
+            var request = new UnlockedRecipesRequest(characterId, accessToken);
+            using var response = await _http.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<Character>(json, Json.DefaultJsonSerializerSettings);
+        }
+    }
+}
