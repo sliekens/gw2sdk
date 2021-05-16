1c041c1d7199ad3d85d5f21c278cfadfdc4e0bdd
Steven Liekens <steven.liekens@gmail.com>
2021-05-17 00:00:14 +0200

    Improve the ItemPrice model
    - Flatten Buyers and Sellers DTOs
    - Rename OpenBuyOrders to TotalDemand
    - Rename OpenSellOrders to TotalSupply

--- GW2SDK/Features/Commerce.Prices/ItemPrice.cs
+++ GW2SDK/Features/Commerce.Prices/ItemPrice.cs
@@ -7,12 +7,24 @@ namespace GW2SDK.Commerce.Prices
     [DataTransferObject]
     public sealed record ItemPrice
     {
+        /// <summary>The item ID.</summary>
         public int Id { get; init; }
 
+        /// <summary>Indicates whether a free to play account can trade this item on the trading post.</summary>
         public bool Whitelisted { get; init; }
 
-        public ItemBuyers Buyers { get; init; } = new();
+        /// <summary>The highest price someone is willing to buy the item for.</summary>
+        public int BestBid { get; init; }
 
-        public ItemSellers Sellers { get; init; } = new();
+        /// <summary>The lowest price someone is willing to sell the item for.</summary>
+        public int BestAsk { get; init; }
+
+        /// <summary>The total number of items demanded (regardless of price level).</summary>
+        /// <remarks>Use the Listing service to get the demand organized by price level.</remarks>
+        public int TotalDemand { get; init; }
+
+        /// <summary>The total number of items supplied (regardless of price level).</summary>
+        /// <remarks>Use the Listing service to get the supply organized by price level.</remarks>
+        public int TotalSupply { get; init; }
     }
 }
