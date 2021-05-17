8ea03bd227b773ce894ea24fb6c0b2ed2bae1911
Steven Liekens <steven.liekens@gmail.com>
2021-05-18 00:32:03 +0200

    Add gem exchange services

---
+++ GW2SDK/Features/Commerce.Exchange/ExchangeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GW2SDK.Commerce.Exchange.Http;
+using GW2SDK.Http;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Commerce.Exchange
+{
+    [PublicAPI]
+    public sealed class ExchangeService
+    {
+        private readonly IExchangeReader _exchangeReader;
+        private readonly HttpClient _http;
+
+        public ExchangeService(HttpClient http, IExchangeReader exchangeReader)
+        {
+            _http = http ?? throw new ArgumentNullException(nameof(http));
+            _exchangeReader = exchangeReader ?? throw new ArgumentNullException(nameof(exchangeReader));
+        }
+
+        public async Task<GemsForGoldExchange> ExchangeGemsForGold(int gemsCount)
+        {
+            var request = new ExchangeGemsForGoldRequest(gemsCount);
+            return await _http.GetResource(request, json => _exchangeReader.GemsForGold.Read(json))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<GoldForGemsExchange> ExchangeGoldForGems(Coin coinsCount)
+        {
+            var request = new ExchangeGoldForGemsRequest(coinsCount);
+            return await _http.GetResource(request, json => _exchangeReader.GoldForGems.Read(json))
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ GW2SDK/Features/Commerce.Exchange/Http/ExchangeGoldForGemsRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using GW2SDK.Http;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Commerce.Exchange.Http
+{
+    [PublicAPI]
+    public sealed class ExchangeGoldForGemsRequest
+    {
+        public ExchangeGoldForGemsRequest(int coinsCount)
+        {
+            CoinsCount = coinsCount;
+        }
+
+        public int CoinsCount { get; }
+
+        public static implicit operator HttpRequestMessage(ExchangeGoldForGemsRequest r)
+        {
+            var search = new QueryBuilder();
+            search.Add("quantity", r.CoinsCount);
+            var location = new Uri($"/v2/commerce/exchange/coins?{search}", UriKind.Relative);
+            return new HttpRequestMessage(HttpMethod.Get, location);
+        }
+    }
+}
