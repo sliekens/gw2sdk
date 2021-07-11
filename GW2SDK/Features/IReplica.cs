f924ee27fa55af9342f87780448365f5155b4523
Steven Liekens <steven.liekens@gmail.com>
2021-07-11 14:57:44 +0200

    Remove usage of MemberNotNullWhen
    It does not work the way I thought

--- GW2SDK/Features/IReplica.cs
+++ GW2SDK/Features/IReplica.cs
@@ -1,12 +1,10 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 
 namespace GW2SDK
 {
     [PublicAPI]
     public interface IReplica<out T> : ITemporal
     {
-        [MemberNotNullWhen(true, nameof(Value))]
         bool HasValue { get; }
 
         T? Value { get; }
