913da3061f45a63a9db4e360ffd04fb8f601d84e
Steven Liekens <steven.liekens@gmail.com>
2021-07-25 18:37:44 +0200

    Add lookup service for specializations

---
+++ GW2SDK/Features/Specializations/Http/SpecializationsRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using GW2SDK.Http;
+using JetBrains.Annotations;
+using static System.Net.Http.HttpMethod;
+
+namespace GW2SDK.Specializations.Http
+{
+    [PublicAPI]
+    public sealed class SpecializationsRequest
+    {
+        public SpecializationsRequest(Language? language)
+        {
+            Language = language;
+        }
+
+        public Language? Language { get; }
+
+        public static implicit operator HttpRequestMessage(SpecializationsRequest r)
+        {
+            var search = new QueryBuilder();
+            search.Add("ids", "all");
+            if (r.Language is not null) search.Add("lang", r.Language.Alpha2Code);
+            var location = new Uri($"/v2/specializations?{search}", UriKind.Relative);
+            return new HttpRequestMessage(Get, location);
+        }
+    }
+}
---
+++ GW2SDK/Features/Specializations/ISpecializationReader.cs
@@ -0,0 +1,11 @@
+﻿using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Specializations
+{
+    [PublicAPI]
+    public interface ISpecializationReader : IJsonReader<Specialization>
+    {
+        IJsonReader<int> Id { get; }
+    }
+}
