b20bc9235db66175a13c7cb3d25c02ec06521dd6
Steven Liekens <steven.liekens@gmail.com>
2021-08-08 01:34:30 +0200

    Add support for MumbleLink

---
+++ GW2SDK.Tests/Features/Mumble/MumbleLinkTest.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading.Tasks;
+using GW2SDK.Mumble;
+using Xunit;
+
+#pragma warning disable CA1416
+namespace GW2SDK.Tests.Features.Mumble
+{
+    public class MumbleLinkTest
+    {
+        [MumbleLinkFact]
+        public void Name_can_be_read_from_Mumble_link()
+        {
+            using var sut = MumbleLink.Open();
+
+            var actual = sut.GetSnapshot();
+
+            Assert.Equal("Guild Wars 2", actual.name);
+        }
+
+        [MumbleLinkFact]
+        public async Task The_link_is_self_updating()
+        {
+            using var sut = MumbleLink.Open();
+
+            var first = sut.GetSnapshot();
+
+            // The link should be updated every frame; you should safely be able to read from it 50 times a second (every 20ms)
+            await Task.Delay(20);
+
+            var second = sut.GetSnapshot();
+
+            Assert.True(second.UiTick > first.UiTick, "MumbleLink should be self-updating");
+        }
+
+        [MumbleLinkFact]
+        public void The_link_provides_context()
+        {
+            using var sut = MumbleLink.Open();
+
+            var snapshot = sut.GetSnapshot();
+            var actual = snapshot.GetContext();
+
+            Assert.True(actual.BuildId > 100_000, "Game build should be over 100,000");
+        }
+    }
+
+    public class MumbleLinkFact : FactAttribute
+    {
+        public MumbleLinkFact()
+        {
+            if (!MumbleLink.IsSupported())
+            {
+                Skip = "MumbleLink is not supported on the current platform.";
+                return;
+            }
+
+            try
+            {
+                using var file = MemoryMappedFile.OpenExisting("MumbleLink", MemoryMappedFileRights.Read);
+            }
+            catch (FileNotFoundException)
+            {
+                Skip = "The MumbleLink is not initialized. Start Mumble -and- Guild Wars 2 before running this test.";
+            }
+        }
+    }
+}
---
+++ GW2SDK/Features/Mumble/MumbleLink.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Buffers;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+#if NET
+using System.Runtime.Versioning;
+
+#endif
+
+namespace GW2SDK.Mumble
+{
+    public sealed class MumbleLink : IDisposable
+    {
+        public const int Length = 0x2000;
+
+        private readonly byte[] _buffer;
+
+        private readonly IntPtr _bufferAddress;
+
+        private readonly MemoryMappedViewStream _content;
+
+        private readonly MemoryMappedFile _file;
+
+        private GCHandle _bufferHandle;
+
+        private MumbleLink(MemoryMappedFile file)
+        {
+            _file = file;
+            _content = file.CreateViewStream(0, Length, MemoryMappedFileAccess.Read);
+            _buffer = ArrayPool<byte>.Shared.Rent(Length);
+            _bufferHandle = GCHandle.Alloc(_buffer, GCHandleType.Pinned);
+            _bufferAddress = _bufferHandle.AddrOfPinnedObject();
+        }
+
+        public void Dispose()
+        {
+            _content.Dispose();
+            _file.Dispose();
+            _bufferHandle.Free();
+            ArrayPool<byte>.Shared.Return(_buffer);
+        }
+
+        public static bool IsSupported()
+        {
+#if NET
+            return OperatingSystem.IsWindows();
+#elif NETSTANDARD
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+#elif NET461
+            return Environment.OSVersion.Platform == PlatformID.Win32NT;
+#endif
+        }
+
+#if NET
+        [SupportedOSPlatform("windows")]
+#endif
+        public static MumbleLink Open(string? name = "MumbleLink")
+        {
+            name ??= "MumbleLink";
+            const long size = Length;
+            var file = MemoryMappedFile.CreateOrOpen(name, size, MemoryMappedFileAccess.Read);
+            return new MumbleLink(file);
+        }
+
+        public Snapshot GetSnapshot()
+        {
+#if NET
+            var buffered = _content.Read(_buffer);
+            if (buffered != Length)
+            {
+                throw new InvalidOperationException($"Expected {Length} bytes but received {buffered}.");
+            }
+#else
+            var buffered = _content.Read(_buffer, 0, Length);
+            if (buffered != Length)
+            {
+                throw new InvalidOperationException($"Expected {Length} bytes but received {buffered}.");
+            }
+#endif
+
+            // Reset the view stream for the next call to GetSnapshot
+            _content.Position = 0;
+
+            return Marshal.PtrToStructure<Snapshot>(_bufferAddress);
+        }
+    }
+}
