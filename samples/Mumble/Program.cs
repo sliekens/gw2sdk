417aacfdfd94123ee11ccaff14c6ec1e97709faa
Steven Liekens <steven.liekens@gmail.com>
2021-08-08 15:41:44 +0200

    Add integrity checks to Mumble

--- samples/Mumble/Program.cs
+++ samples/Mumble/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using GW2SDK.Json;
 using GW2SDK.Mumble;
 
 namespace Mumble
@@ -48,11 +49,32 @@ namespace Mumble
             WaitHandle.WaitAll(new[] { cancellationToken.WaitHandle });
         }
 
-        private void CallBack(object? state)
+        private void CallBack(object state)
         {
             var snapshot = (Snapshot) state;
-            var pos = snapshot.AvatarPositionMeters;
-            Console.WriteLine("Update: {0}, Right: {1}, Up: {2}, Front: {3}", snapshot.UiTick, pos[0], pos[1], pos[2]);
+            var pos = snapshot.AvatarPosition;
+
+            if (!snapshot.TryGetIdentity(out var identity, MissingMemberBehavior.Error))
+            {
+                return;
+            }
+
+            if (!snapshot.TryGetContext(out var context))
+            {
+                return;
+            }
+
+            Console.WriteLine("Update {0}: {1}, the {2} {3} is {4} on {5} in Map: {6}, Position: {{ Right = {7}, Up = {8}, Front = {9} }}",
+                snapshot.UiTick,
+                identity.Name,
+                identity.Race,
+                identity.Profession,
+                context.UiState.HasFlag(UiState.IsInCombat) ? "fighting" : "traveling",
+                context.IsMounted ? context.GetMount() : "foot",
+                identity.MapId,
+                pos[0],
+                pos[1],
+                pos[2]);
         }
     }
 }
