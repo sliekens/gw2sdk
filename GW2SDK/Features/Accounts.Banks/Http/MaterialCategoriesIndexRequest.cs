e1fd74bba4223ae0e2cf4db673e9d13eb8cafa36
Steven Liekens <steven.liekens@gmail.com>
2021-08-27 20:56:38 +0200

    Add lookup service for materials

---
+++ GW2SDK/Features/Accounts.Banks/Http/MaterialCategoriesIndexRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Accounts.Banks.Http
+{
+    [PublicAPI]
+    public sealed class MaterialCategoriesIndexRequest
+    {
+        public static implicit operator HttpRequestMessage(MaterialCategoriesIndexRequest _)
+        {
+            var location = new Uri("/v2/materials", UriKind.Relative);
+            return new HttpRequestMessage(HttpMethod.Get, location);
+        }
+    }
+}
---
+++ GW2SDK/Features/Accounts.Banks/Http/MaterialCategoryByIdRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using GW2SDK.Http;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Accounts.Banks.Http
+{
+    [PublicAPI]
+    public sealed class MaterialCategoryByIdRequest
+    {
+        public MaterialCategoryByIdRequest(int materialCategoryId, Language? language)
+        {
+            MaterialCategoryId = materialCategoryId;
+            Language = language;
+        }
+
+        public int MaterialCategoryId { get; }
+
+        public Language? Language { get; }
+
+        public static implicit operator HttpRequestMessage(MaterialCategoryByIdRequest r)
+        {
+            var search = new QueryBuilder();
+            search.Add("id", r.MaterialCategoryId);
+            if (r.Language is not null) search.Add("lang", r.Language.Alpha2Code);
+            var location = new Uri($"/v2/materials?{search}", UriKind.Relative);
+            return new HttpRequestMessage(HttpMethod.Get, location);
+        }
+    }
+}
