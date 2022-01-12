649562d06181b336b9a999dbecdccdfdd56b9ab3
Steven Liekens <steven.liekens@gmail.com>
2022-01-12 22:20:53 +0100

    Add wallet lookup service

---
+++ GW2SDK/Features/Accounts.Wallet/WalletReader.cs
@@ -0,0 +1,12 @@
+﻿using GW2SDK.Accounts.Wallet;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Features.Accounts.Wallet
+{
+    [PublicAPI]
+    public sealed class WalletReader : IWalletReader
+    {
+        public IJsonReader<CurrencyAmount> CurrencyAmount { get; } = new CurrencyAmountReader();
+    }
+}
---
+++ GW2SDK/Features/Accounts.Wallet/WalletService.cs
@@ -0,0 +1,52 @@
+﻿using GW2SDK.Accounts.Wallet;
+using GW2SDK.Accounts.Wallet.Http;
+using GW2SDK.Annotations;
+using GW2SDK.Http;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GW2SDK.Features.Accounts.Wallet
+{
+    [PublicAPI]
+    public sealed class WalletService
+    {
+        private readonly IWalletReader walletReader;
+
+        private readonly HttpClient http;
+
+        private readonly MissingMemberBehavior missingMemberBehavior;
+
+        public WalletService(
+            HttpClient http,
+            IWalletReader walletReader,
+            MissingMemberBehavior missingMemberBehavior
+        )
+        {
+            this.http = http ?? throw new ArgumentNullException(nameof(http));
+            this.walletReader = walletReader ?? throw new ArgumentNullException(nameof(walletReader));
+            this.missingMemberBehavior = missingMemberBehavior;
+        }
+
+        [Scope(Permission.Wallet)]
+#if NET
+        public async Task<IReplica<IReadOnlySet<CurrencyAmount>>> GetWallet(string? accessToken, CancellationToken cancellationToken = default)
+#else
+        public async Task<IReplica<IReadOnlyCollection<CurrencyAmount>>> GetWallet(
+            string? accessToken,
+            CancellationToken cancellationToken = default
+        )
+#endif
+        {
+            var request = new WalletRequest(accessToken);
+            return await http.GetResourcesSetSimple(request,
+                    json => walletReader.CurrencyAmount.ReadArray(json, missingMemberBehavior),
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
