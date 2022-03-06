bfb39c1390c6ffcb6c86c700b12fbf8533a997b0
Steven Liekens <steven.liekens@gmail.com>
2022-03-06 15:34:37 +0100

    Rewrite dev container scripts

---
+++ .devcontainer/update-scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Run this script to fetch the latest library scripts from the VS Code and Codespaces teams
+#
+# Syntax: ./update-scripts.sh
+
+scripts="common-debian,docker-debian,dotnet-debian,git-from-src-debian,git-lfs-debian,github-debian,powershell-debian"
+devcontainer=$(dirname $(realpath $0))
+mkdir -p $devcontainer/library-scripts && cd $_
+curl --fail-early --remote-name-all\
+  --fail --silent --show-error "https://raw.githubusercontent.com/microsoft/vscode-dev-containers/main/script-library/{$scripts}.sh"
