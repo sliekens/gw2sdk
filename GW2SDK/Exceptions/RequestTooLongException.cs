7050637ec8e79810fd7f04688971a8d939e07291
Steven Liekens <steven.liekens@gmail.com>
2022-03-26 03:06:59 +0100

    Throw when URI exceeds 2000 characters

---
+++ GW2SDK/Exceptions/RequestTooLongException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace GW2SDK
+{
+    [PublicAPI]
+    public sealed class RequestTooLongException : InvalidOperationException
+    {
+        public RequestTooLongException()
+        {
+        }
+
+        public RequestTooLongException(string? message)
+            : base(message)
+        {
+        }
+
+        public RequestTooLongException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
