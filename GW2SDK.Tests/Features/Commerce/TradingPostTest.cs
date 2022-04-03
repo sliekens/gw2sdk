9bf046f020fdb4ee8b5fd9c1f6c28c2bbfad90e7
Steven Liekens <steven.liekens@gmail.com>
2022-04-03 16:26:34 +0200

    Add convenience members to item prices and order books
    - Best and Total bids/asks for the order book
    - BidAskSpread for both

--- GW2SDK.Tests/Features/Commerce/TradingPostTest.cs
+++ GW2SDK.Tests/Features/Commerce/TradingPostTest.cs
@@ -5,214 +5,327 @@ using GW2SDK.Commerce;
 using GW2SDK.Tests.TestInfrastructure;
 using Xunit;
 
-namespace GW2SDK.Tests.Features.Commerce
+namespace GW2SDK.Tests.Features.Commerce;
+
+public class TradingPostTest
 {
-    public class TradingPostTest
+    [Fact]
+    public async Task It_can_calculate_gold_for_gems()
     {
-        [Fact]
-        public async Task It_can_calculate_gold_for_gems()
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        var coins = new Coin(100, 0, 0);
+
+        var actual = await sut.ExchangeGoldForGems(coins);
+
+        Assert.True(actual.Value.GemsToReceive > 0, "100 gold should be worth some gems.");
+        Assert.True(actual.Value.CoinsPerGem > 0, "Gems can't be free.");
+    }
+
+    [Fact]
+    public async Task It_can_calculate_gems_for_gold()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        const int gems = 800;
+
+        var actual = await sut.ExchangeGemsForGold(gems);
+
+        Assert.True(actual.Value.CoinsToReceive > 10000, "800 gems should be worth some gold.");
+        Assert.True(actual.Value.CoinsPerGem > 0, "Gems can't be free.");
+    }
+
+    [Fact]
+    public async Task It_can_get_all_item_price_ids()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        var actual = await sut.GetItemPricesIndex();
+
+        Assert.Equal(actual.Context.ResultTotal, actual.Values.Count);
+    }
+
+    [Fact]
+    public async Task It_can_get_an_item_price_by_id()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        const int itemId = 24;
+
+        var actual = await sut.GetItemPriceById(itemId);
+
+        Assert.True(actual.HasValue);
+        var value = actual.Value;
+        Assert.Equal(itemId, value.Id);
+        Assert.True(value.TotalSupply > 0);
+        Assert.True(value.BestAsk > 0);
+        Assert.True(value.TotalDemand > 0);
+        Assert.True(value.BestBid > 0);
+        Assert.Equal(value.BestAsk - value.BestBid, value.BidAskSpread);
+    }
+
+    [Fact]
+    public async Task It_can_get_item_prices_by_id()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        var ids = new HashSet<int>
         {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
+            24,
+            19699,
+            35984
+        };
 
-            var coins = new Coin(100, 0, 0);
+        var actual = await sut.GetItemPricesByIds(ids)
+            .ToListAsync();
 
-            var actual = await sut.ExchangeGoldForGems(coins);
+        Assert.Collection(actual.Values(),
+            first => Assert.Equal(24, first.Id),
+            second => Assert.Equal(19699, second.Id),
+            third => Assert.Equal(35984, third.Id));
+    }
 
-            Assert.True(actual.Value.GemsToReceive > 0, "100 gold should be worth some gems.");
-            Assert.True(actual.Value.CoinsPerGem > 0, "Gems can't be free.");
-        }
+    [Fact(Skip =
+        "This test is best used interactively, otherwise it will hit rate limits in this as well as other tests.")]
+    public async Task It_can_get_all_item_prices()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
 
-        [Fact]
-        public async Task It_can_calculate_gems_for_gold()
+        await foreach (var actual in sut.GetItemPrices())
         {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
+            Assert.True(actual.HasValue);
 
-            const int gems = 800;
+            var value = actual.Value;
 
-            var actual = await sut.ExchangeGemsForGold(gems);
-
-            Assert.True(actual.Value.CoinsToReceive > 10000, "800 gems should be worth some gold.");
-            Assert.True(actual.Value.CoinsPerGem > 0, "Gems can't be free.");
-        }
-
-        [Fact]
-        public async Task It_can_get_all_item_price_ids()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
-
-            var actual = await sut.GetItemPricesIndex();
-
-            Assert.Equal(actual.Context.ResultTotal, actual.Values.Count);
-        }
-
-        [Fact]
-        public async Task It_can_get_an_item_price_by_id()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
-
-            const int itemId = 24;
-
-            var actual = await sut.GetItemPriceById(itemId);
-
-            Assert.Equal(itemId, actual.Value.Id);
-        }
-
-        [Fact]
-        public async Task It_can_get_item_prices_by_id()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
-
-            var ids = new HashSet<int>
+            Assert.True(value.Id > 0);
+            if (value.TotalSupply == 0)
             {
-                24,
-                19699,
-                35984
-            };
-
-            var actual = await sut.GetItemPricesByIds(ids)
-                .ToListAsync();
-
-            Assert.Collection(actual.Values(),
-                first => Assert.Equal(24, first.Id),
-                second => Assert.Equal(19699, second.Id),
-                third => Assert.Equal(35984, third.Id));
-        }
-
-        [Fact(Skip =
-            "This test is best used interactively, otherwise it will hit rate limits in this as well as other tests.")]
-        public async Task It_can_get_all_item_prices()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
-
-            await foreach (var actual in sut.GetItemPrices())
+                Assert.True(value.BestAsk == Coin.Zero);
+            }
+            else
             {
-                Assert.InRange(actual.Value.Id, 1, int.MaxValue);
+                Assert.True(value.BestAsk > Coin.Zero);
+            }
+
+            if (value.TotalDemand == 0)
+            {
+                Assert.True(value.BestBid == Coin.Zero);
+            }
+            else
+            {
+                Assert.True(value.BestBid > Coin.Zero);
+            }
+
+            if (value is { TotalDemand: 0 } or { TotalSupply: 0 })
+            {
+                Assert.Equal(Coin.Zero, value.BidAskSpread);
+            }
+            else
+            {
+                Assert.Equal(value.BestAsk - value.BestBid, value.BidAskSpread);
             }
         }
+    }
 
-        [Fact]
-        public async Task It_can_get_all_order_book_ids()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_all_order_book_ids()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
 
-            var actual = await sut.GetOrderBooksIndex();
+        var actual = await sut.GetOrderBooksIndex();
 
-            Assert.Equal(actual.Context.ResultTotal, actual.Values.Count);
-        }
+        Assert.Equal(actual.Context.ResultTotal, actual.Values.Count);
+    }
 
-        [Fact]
-        public async Task It_can_get_an_order_book_by_id()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_an_order_book_by_id()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
 
-            const int itemId = 24;
+        const int itemId = 24;
 
-            var actual = await sut.GetOrderBookById(itemId);
+        var actual = await sut.GetOrderBookById(itemId);
 
-            Assert.Equal(itemId, actual.Value.Id);
-        }
+        Assert.True(actual.HasValue);
+        var value = actual.Value;
+        Assert.Equal(itemId, value.Id);
 
-        [Fact]
-        public async Task It_can_get_order_books_by_id()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
+        Assert.True(value.TotalSupply > 0);
+        Assert.True(value.BestAsk > Coin.Zero);
+        Assert.True(value.TotalDemand > 0);
+        Assert.True(value.BestBid > Coin.Zero);
+        Assert.Equal(value.BestAsk - value.BestBid, value.BidAskSpread);
 
-            var ids = new HashSet<int>
+        Assert.Equal(value.TotalDemand, value.Demand.Sum(bid => bid.Quantity));
+        Assert.Equal(value.TotalSupply, value.Supply.Sum(ask => ask.Quantity));
+
+        Assert.NotEmpty(value.Supply);
+        Assert.All(value.Supply,
+            line =>
             {
-                24,
-                19699,
-                35984
-            };
+                Assert.True(line.UnitPrice > Coin.Zero);
+                Assert.True(line.Quantity > 0);
+                Assert.True(line.Listings > 0);
+            });
 
-            var actual = await sut.GetOrderBooksByIds(ids)
-                .ToListAsync();
-
-            Assert.Collection(actual.Values(),
-                first => Assert.Equal(24, first.Id),
-                second => Assert.Equal(19699, second.Id),
-                third => Assert.Equal(35984, third.Id));
-        }
-
-        [Fact(Skip =
-            "This test is best used interactively, otherwise it will hit rate limits in this as well as other tests.")]
-        public async Task It_can_get_all_order_books()
-        {
-            await using var services = new Composer();
-            var sut = services.Resolve<TradingPost>();
-
-            await foreach (var actual in sut.GetOrderBooks())
+        Assert.NotEmpty(value.Demand);
+        Assert.All(value.Demand,
+            line =>
             {
-                Assert.InRange(actual.Value.Id, 1, int.MaxValue);
+                Assert.True(line.UnitPrice > Coin.Zero);
+                Assert.True(line.Quantity > 0);
+                Assert.True(line.Listings > 0);
+            });
+    }
+
+    [Fact]
+    public async Task It_can_get_order_books_by_id()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        var ids = new HashSet<int>
+        {
+            24,
+            19699,
+            35984
+        };
+
+        var actual = await sut.GetOrderBooksByIds(ids)
+            .ToListAsync();
+
+        Assert.Collection(actual.Values(),
+            first => Assert.Equal(24, first.Id),
+            second => Assert.Equal(19699, second.Id),
+            third => Assert.Equal(35984, third.Id));
+    }
+
+    [Fact(Skip =
+        "This test is best used interactively, otherwise it will hit rate limits in this as well as other tests.")]
+    public async Task It_can_get_all_order_books()
+    {
+        await using var services = new Composer();
+        var sut = services.Resolve<TradingPost>();
+
+        await foreach (var actual in sut.GetOrderBooks())
+        {
+            Assert.True(actual.HasValue);
+
+            var value = actual.Value;
+
+            Assert.True(value.Id > 0);
+            if (value.TotalSupply == 0)
+            {
+                Assert.Null(value.BestAsk);
+                Assert.Empty(value.Supply);
+            }
+            else
+            {
+                Assert.True(value.BestAsk > Coin.Zero);
+                Assert.NotEmpty(value.Supply);
+                Assert.All(value.Supply,
+                    line =>
+                    {
+                        Assert.True(line.UnitPrice > Coin.Zero);
+                        Assert.True(line.Quantity > 0);
+                        Assert.True(line.Listings > 0);
+                    });
+            }
+
+            if (value.TotalDemand == 0)
+            {
+                Assert.Null(value.BestBid);
+                Assert.Empty(value.Demand);
+            }
+            else
+            {
+                Assert.True(value.BestBid > Coin.Zero);
+                Assert.NotEmpty(value.Demand);
+                Assert.All(value.Demand,
+                    line =>
+                    {
+                        Assert.True(line.UnitPrice > Coin.Zero);
+                        Assert.True(line.Quantity > 0);
+                        Assert.True(line.Listings > 0);
+                    });
+            }
+
+            if (value is { TotalDemand: 0 } or { TotalSupply: 0 })
+            {
+                Assert.Equal(Coin.Zero, value.BidAskSpread);
+            }
+            else
+            {
+                Assert.Equal(value.BestAsk - value.BestBid, value.BidAskSpread);
             }
         }
+    }
 
-        [Fact]
-        public async Task It_can_get_your_delivery_box()
-        {
-            await using var service = new Composer();
-            var accessToken = service.Resolve<ApiKeyFull>();
-            var sut = service.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_your_delivery_box()
+    {
+        await using var service = new Composer();
+        var accessToken = service.Resolve<ApiKeyFull>();
+        var sut = service.Resolve<TradingPost>();
 
-            var deliveryBox = await sut.GetDeliveryBox(accessToken.Key);
+        var deliveryBox = await sut.GetDeliveryBox(accessToken.Key);
 
-            Assert.True(deliveryBox.HasValue);
-        }
+        Assert.True(deliveryBox.HasValue);
+    }
 
-        [Fact]
-        public async Task It_can_get_your_current_bids()
-        {
-            await using var service = new Composer();
-            var accessToken = service.Resolve<ApiKeyFull>();
-            var sut = service.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_your_current_bids()
+    {
+        await using var service = new Composer();
+        var accessToken = service.Resolve<ApiKeyFull>();
+        var sut = service.Resolve<TradingPost>();
 
-            var bids = await sut.GetBuyOrders(0, 200, accessToken.Key);
+        var bids = await sut.GetBuyOrders(0, 200, accessToken.Key);
 
-            Assert.True(bids.HasValues);
-        }
+        Assert.True(bids.HasValues);
+    }
 
-        [Fact]
-        public async Task It_can_get_your_current_asks()
-        {
-            await using var service = new Composer();
-            var accessToken = service.Resolve<ApiKeyFull>();
-            var sut = service.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_your_current_asks()
+    {
+        await using var service = new Composer();
+        var accessToken = service.Resolve<ApiKeyFull>();
+        var sut = service.Resolve<TradingPost>();
 
-            var bids = await sut.GetSellOrders(0, 200, accessToken.Key);
+        var bids = await sut.GetSellOrders(0, 200, accessToken.Key);
 
-            Assert.True(bids.HasValues);
-        }
+        Assert.True(bids.HasValues);
+    }
 
-        [Fact]
-        public async Task It_can_get_your_purchases()
-        {
-            await using var service = new Composer();
-            var accessToken = service.Resolve<ApiKeyFull>();
-            var sut = service.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_your_purchases()
+    {
+        await using var service = new Composer();
+        var accessToken = service.Resolve<ApiKeyFull>();
+        var sut = service.Resolve<TradingPost>();
 
-            var bids = await sut.GetPurchases(0, 200, accessToken.Key);
+        var bids = await sut.GetPurchases(0, 200, accessToken.Key);
 
-            Assert.True(bids.HasValues);
-        }
+        Assert.True(bids.HasValues);
+    }
 
-        [Fact]
-        public async Task It_can_get_your_sales()
-        {
-            await using var service = new Composer();
-            var accessToken = service.Resolve<ApiKeyFull>();
-            var sut = service.Resolve<TradingPost>();
+    [Fact]
+    public async Task It_can_get_your_sales()
+    {
+        await using var service = new Composer();
+        var accessToken = service.Resolve<ApiKeyFull>();
+        var sut = service.Resolve<TradingPost>();
 
-            var bids = await sut.GetSales(0, 200, accessToken.Key);
+        var bids = await sut.GetSales(0, 200, accessToken.Key);
 
-            Assert.True(bids.HasValues);
-        }
+        Assert.True(bids.HasValues);
     }
 }
