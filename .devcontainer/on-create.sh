5bd9f0e884b8ffad146e86a38682fce073543ff4
Steven Liekens <steven.liekens@gmail.com>
2022-04-15 14:54:24 +0000

    Make web sample work in devcontainer

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This script is executed inside the dev container when it is first created.
+# Useful for just-in-time config.
+#
+
+# Generate a dev cert for web samples
+dotnet dev-certs https
