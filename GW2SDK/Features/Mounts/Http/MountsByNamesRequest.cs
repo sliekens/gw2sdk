05192ec480d9e678ba25e8ce81c6a5a4be032890
Steven Liekens <steven.liekens@gmail.com>
2022-04-17 16:09:44 +0200

    Implement Turtle mount lookup

--- GW2SDK/Features/Mounts/Http/MountsByNamesRequest.cs
+++ GW2SDK/Features/Mounts/Http/MountsByNamesRequest.cs
@@ -30,7 +30,7 @@ public sealed class MountsByNamesRequest
     public static implicit operator HttpRequestMessage(MountsByNamesRequest r)
     {
         QueryBuilder search = new();
-        search.Add("ids", r.MountNames.Select(name => FormatMountName(name)));
+        search.Add("ids", r.MountNames.Select(name => MountNameFormatter.FormatMountName(name)));
         var request = Template with
         {
             AcceptLanguage = r.Language?.Alpha2Code,
@@ -38,18 +38,4 @@ public sealed class MountsByNamesRequest
         };
         return request.Compile();
     }
-
-    private static string FormatMountName(MountName mountName) =>
-        mountName switch
-        {
-            MountName.Griffon => "griffon",
-            MountName.Jackal => "jackal",
-            MountName.Raptor => "raptor",
-            MountName.RollerBeetle => "roller_beetle",
-            MountName.Skimmer => "skimmer",
-            MountName.Skyscale => "skyscale",
-            MountName.Springer => "springer",
-            MountName.Warclaw => "warclaw",
-            _ => throw new NotSupportedException("Could not format mount name.")
-        };
 }
