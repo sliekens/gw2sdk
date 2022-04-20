8f9b752e85144c09c4107923857abba52433d109
Steven Liekens <steven.liekens@gmail.com>
2022-04-20 23:38:44 +0200

    Add cats lookup

---
+++ GW2SDK/Features/Home/Http/CatByIdRequest.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GW2SDK.Home.Json;
+using GW2SDK.Home.Models;
+using GW2SDK.Http;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Home.Http;
+
+[PublicAPI]
+public sealed class CatByIdRequest : IHttpRequest<IReplica<Cat>>
+{
+    private static readonly HttpRequestMessageTemplate Template =
+        new(HttpMethod.Get, "/v2/home/cats") { AcceptEncoding = "gzip" };
+
+    public CatByIdRequest(int catId)
+    {
+        CatId = catId;
+    }
+
+    public int CatId { get; }
+
+    public MissingMemberBehavior MissingMemberBehavior { get; init; }
+
+    public async Task<IReplica<Cat>> SendAsync(
+        HttpClient httpClient,
+        CancellationToken cancellationToken
+    )
+    {
+        QueryBuilder search = new();
+        search.Add("id", CatId);
+        var request = Template with { Arguments = search };
+
+        using var response = await httpClient.SendAsync(
+                request.Compile(),
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken
+                )
+            .ConfigureAwait(false);
+
+        await response.EnsureResult(cancellationToken).ConfigureAwait(false);
+
+        using var json = await response.Content.ReadAsJsonAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        var value = CatReader.Read(json.RootElement, MissingMemberBehavior);
+        return new Replica<Cat>(
+            response.Headers.Date.GetValueOrDefault(),
+            value,
+            response.Content.Headers.Expires,
+            response.Content.Headers.LastModified
+            );
+    }
+}
---
+++ GW2SDK/Features/Home/Http/CatsByPageRequest.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GW2SDK.Home.Json;
+using GW2SDK.Home.Models;
+using GW2SDK.Http;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Home.Http;
+
+[PublicAPI]
+public sealed class CatsByPageRequest : IHttpRequest<IReplicaPage<Cat>>
+{
+    private static readonly HttpRequestMessageTemplate Template =
+        new(HttpMethod.Get, "/v2/home/cats") { AcceptEncoding = "gzip" };
+
+    public CatsByPageRequest(int pageIndex)
+    {
+        PageIndex = pageIndex;
+    }
+
+    public int PageIndex { get; }
+
+    public int? PageSize { get; init; }
+
+    public MissingMemberBehavior MissingMemberBehavior { get; init; }
+
+    public async Task<IReplicaPage<Cat>> SendAsync(
+        HttpClient httpClient,
+        CancellationToken cancellationToken
+    )
+    {
+        QueryBuilder search = new();
+        search.Add("page", PageIndex);
+        if (PageSize.HasValue)
+        {
+            search.Add("page_size", PageSize.Value);
+        }
+
+        var request = Template with { Arguments = search };
+
+        using var response = await httpClient.SendAsync(
+                request.Compile(),
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken
+                )
+            .ConfigureAwait(false);
+
+        await response.EnsureResult(cancellationToken).ConfigureAwait(false);
+
+        using var json = await response.Content.ReadAsJsonAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        var value = json.RootElement.GetSet(entry => CatReader.Read(entry, MissingMemberBehavior));
+        return new ReplicaPage<Cat>(
+            response.Headers.Date.GetValueOrDefault(),
+            value,
+            response.Headers.GetPageContext(),
+            response.Content.Headers.Expires,
+            response.Content.Headers.LastModified
+            );
+    }
+}
