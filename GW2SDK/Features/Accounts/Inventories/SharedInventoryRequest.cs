faf66eb3339788da81fc6204a0e389c017ee5dbd
Steven Liekens <steven.liekens@gmail.com>
2022-04-24 17:21:20 +0200

    Add shared inventory lookup

---
+++ GW2SDK/Features/Accounts/Inventories/SharedInventoryRequest.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GW2SDK.Http;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK.Accounts.Inventories;
+
+[PublicAPI]
+public sealed class SharedInventoryRequest : IHttpRequest<IReplica<Inventory>>
+{
+    private static readonly HttpRequestMessageTemplate Template =
+        new(HttpMethod.Get, "/v2/account/inventory") { AcceptEncoding = "gzip" };
+
+    public string? AccessToken { get; init; }
+
+    public MissingMemberBehavior MissingMemberBehavior { get; init; }
+
+    public async Task<IReplica<Inventory>> SendAsync(
+        HttpClient httpClient,
+        CancellationToken cancellationToken
+    )
+    {
+        var request = Template with { BearerToken = AccessToken };
+
+        using var response = await httpClient.SendAsync(
+                request.Compile(),
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken
+                )
+            .ConfigureAwait(false);
+
+        await response.EnsureResult(cancellationToken).ConfigureAwait(false);
+
+        using var json = await response.Content.ReadAsJsonAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        var value = InventoryReader.Read(json.RootElement, MissingMemberBehavior);
+        return new Replica<Inventory>(
+            response.Headers.Date.GetValueOrDefault(),
+            value,
+            response.Content.Headers.Expires,
+            response.Content.Headers.LastModified
+            );
+    }
+}
---
+++ GW2SDK/Features/InventoryQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using GW2SDK.Accounts.Inventories;
+using GW2SDK.Http;
+using GW2SDK.Json;
+using JetBrains.Annotations;
+
+namespace GW2SDK;
+
+[PublicAPI]
+public sealed class InventoryQuery
+{
+    private readonly HttpClient http;
+
+    public InventoryQuery(HttpClient http)
+    {
+        this.http = http.WithDefaults() ?? throw new ArgumentNullException(nameof(http));
+    }
+
+    public Task<IReplica<Inventory>> GetSharedInventory(
+        string? accessToken,
+        MissingMemberBehavior missingMemberBehavior = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var request = new SharedInventoryRequest
+        {
+            AccessToken = accessToken,
+            MissingMemberBehavior = missingMemberBehavior
+        };
+        return request.SendAsync(http, cancellationToken);
+    }
+}
