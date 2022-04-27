a7c198e69f16924d5676d0e3895e9ee0728dd663
Steven Liekens <steven.liekens@gmail.com>
2022-04-27 19:35:07 +0000

    Add Node to devcontainer

--- .devcontainer/update-scripts.sh
+++ .devcontainer/update-scripts.sh
@@ -4,7 +4,7 @@
 #
 # Syntax: ./update-scripts.sh
 
-scripts="common-debian,docker-debian,dotnet-debian,git-from-src-debian,git-lfs-debian,github-debian,powershell-debian"
+scripts="common-debian,docker-debian,dotnet-debian,git-from-src-debian,git-lfs-debian,github-debian,powershell-debian,node-debian"
 devcontainer=$(dirname $(realpath $0))
 mkdir -p $devcontainer/library-scripts && cd $_
 curl --fail-early --remote-name-all\
