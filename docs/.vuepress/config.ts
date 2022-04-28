13806467cebe2f449e4e4c25a71a6545be76422a
Steven Liekens <steven.liekens@gmail.com>
2022-04-28 22:04:14 +0000

    Add docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vuepress/config";
+
+export default defineConfig({
+  title: 'GW2SDK',
+  description: 'A .NET code library for interacting with the Guild Wars 2 API and game client.',
+  base: '/gw2sdk/'
+});
