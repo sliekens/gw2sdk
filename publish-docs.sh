0da2ccb07deff3c1a268c0e4b6f602f01b60ab5d
Steven Liekens <steven.liekens@gmail.com>
2022-04-28 23:35:01 +0000

    Fix git rm command

--- publish-docs.sh
+++ publish-docs.sh
@@ -4,7 +4,7 @@ npm run build
 git stash push -u -- docs/.vuepress/dist
 git fetch origin gh-pages
 git checkout gh-pages
-git rm -rf *
+git rm -rf .
 git stash pop
 git add .
 git mv docs/.vuepress/dist/* .
