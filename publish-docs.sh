f1620ccca38cc98398c2e3c9f00b385bde1bd53c
Steven Liekens <steven.liekens@gmail.com>
2022-04-28 23:21:58 +0000

    Fix gh-pages workflow

---
+++ publish-docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+npm ci
+npm run build
+git stash push -u -- docs/.vuepress/dist
+git fetch origin gh-pages
+git checkout gh-pages
+git rm -rf *
+git stash pop
+git add .
+git mv docs/.vuepress/dist/* .
+git commit -m "Update docs"
+git push
