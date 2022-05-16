fa757d2adb701f0cf7983ddaefd9c67c209e10d6
Steven <steven.liekens@gmail.com>
2022-05-16 18:43:20 +0200

    Pretty print test data downloader exceptions

--- GW2SDK.TestDataHelper/Program.cs
+++ GW2SDK.TestDataHelper/Program.cs
@@ -14,110 +14,122 @@ public class Program
         Directory.CreateDirectory(outDir);
         await using var services = new Container();
 
-        await AnsiConsole.Progress()
-            .StartAsync(
-                async ctx =>
-                {
-                    var achievements = ctx.AddTask("Downloading achievements.");
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "achievements.json.gz")))
+        try
+        {
+            await AnsiConsole.Progress()
+                .StartAsync(
+                    async ctx =>
                     {
-                        var service = services.Resolve<JsonAchievementService>();
-                        var documents = await service.GetAllJsonAchievements(Update(achievements));
-                        foreach (var document in documents)
+                        var achievements = ctx.AddTask("Downloading achievements.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "achievements.json.gz")))
                         {
-                            await file.WriteLineAsync(document);
-                        }
-                    }
-
-                    var items = ctx.AddTask("Downloading items.");
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "items.json.gz")))
-                    {
-                        var service = services.Resolve<JsonItemService>();
-                        var documents = await service.GetAllJsonItems(Update(items));
-                        foreach (var document in documents)
-                        {
-                            await file.WriteLineAsync(document);
-                        }
-                    }
-
-                    var recipes = ctx.AddTask("Downloading recipes.");
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "recipes.json.gz")))
-                    {
-                        var service = services.Resolve<JsonRecipeService>();
-                        var documents = await service.GetAllJsonRecipes(Update(recipes));
-                        foreach (var document in documents)
-                        {
-                            await file.WriteLineAsync(document);
-                        }
-                    }
-
-                    var skins = ctx.AddTask("Downloading skins.");
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "skins.json.gz")))
-                    {
-                        var service = services.Resolve<JsonSkinService>();
-                        var documents = await service.GetAllJsonSkins(Update(skins));
-                        foreach (var document in documents)
-                        {
-                            await file.WriteLineAsync(document);
-                        }
-                    }
-
-                    var floors1 = ctx.AddTask("Downloading floors.").MaxValue(2);
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "continents_1_floors.json.gz")))
-                    {
-                        var service = services.Resolve<JsonFloorService>();
-                        var documents = await service.GetAllJsonFloors(1);
-                        foreach (var document in documents)
-                        {
-                            await file.WriteLineAsync(document);
+                            var service = services.Resolve<JsonAchievementService>();
+                            var documents =
+                                await service.GetAllJsonAchievements(Update(achievements));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
                         }
 
-                        floors1.Increment(1);
-                    }
-
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "continents_2_floors.json.gz")))
-                    {
-                        var service = services.Resolve<JsonFloorService>();
-                        var documents = await service.GetAllJsonFloors(2);
-                        foreach (var document in documents)
+                        var items = ctx.AddTask("Downloading items.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "items.json.gz")))
                         {
-                            await file.WriteLineAsync(document);
+                            var service = services.Resolve<JsonItemService>();
+                            var documents = await service.GetAllJsonItems(Update(items));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
                         }
 
-                        floors1.Increment(1);
-                    }
-
-                    var prices = ctx.AddTask("Downloading prices.");
-                    await using (var
-                        file = CreateTextCompressed(Path.Combine(outDir, "prices.json.gz")))
-                    {
-                        var service = services.Resolve<JsonItemPriceService>();
-                        var documents = await service.GetJsonItemPrices(Update(prices));
-                        foreach (var document in documents)
+                        var recipes = ctx.AddTask("Downloading recipes.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "recipes.json.gz")))
                         {
-                            await file.WriteLineAsync(document);
+                            var service = services.Resolve<JsonRecipeService>();
+                            var documents = await service.GetAllJsonRecipes(Update(recipes));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
+                        }
+
+                        var skins = ctx.AddTask("Downloading skins.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "skins.json.gz")))
+                        {
+                            var service = services.Resolve<JsonSkinService>();
+                            var documents = await service.GetAllJsonSkins(Update(skins));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
+                        }
+
+                        var floors1 = ctx.AddTask("Downloading floors.").MaxValue(2);
+                        await using (var file = CreateTextCompressed(
+                                Path.Combine(outDir, "continents_1_floors.json.gz")
+                            ))
+                        {
+                            var service = services.Resolve<JsonFloorService>();
+                            var documents = await service.GetAllJsonFloors(1);
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
+
+                            floors1.Increment(1);
+                        }
+
+                        await using (var file = CreateTextCompressed(
+                                Path.Combine(outDir, "continents_2_floors.json.gz")
+                            ))
+                        {
+                            var service = services.Resolve<JsonFloorService>();
+                            var documents = await service.GetAllJsonFloors(2);
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
+
+                            floors1.Increment(1);
+                        }
+
+                        var prices = ctx.AddTask("Downloading prices.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "prices.json.gz")))
+                        {
+                            var service = services.Resolve<JsonItemPriceService>();
+                            var documents = await service.GetJsonItemPrices(Update(prices));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
+                        }
+
+                        var orders = ctx.AddTask("Downloading orders.");
+                        await using (var file =
+                            CreateTextCompressed(Path.Combine(outDir, "listings.json.gz")))
+                        {
+                            var service = services.Resolve<JsonOrderBookService>();
+                            var documents = await service.GetJsonOrderBooks(Update(orders));
+                            foreach (var document in documents)
+                            {
+                                await file.WriteLineAsync(document);
+                            }
                         }
                     }
+                );
+        }
+        catch (Exception crash)
+        {
+            AnsiConsole.WriteException(crash);
+        }
 
-                    var orders = ctx.AddTask("Downloading orders.");
-                    await using (var file =
-                        CreateTextCompressed(Path.Combine(outDir, "listings.json.gz")))
-                    {
-                        var service = services.Resolve<JsonOrderBookService>();
-                        var documents = await service.GetJsonOrderBooks(Update(orders));
-                        foreach (var document in documents)
-                        {
-                            await file.WriteLineAsync(document);
-                        }
-                    }
-                }
-            );
+        ;
     }
 
     private static Progress<ICollectionContext> Update(ProgressTask progressTask) =>
