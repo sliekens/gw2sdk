2a24981a0cad6582813dd95c70f97199dc4a3538
Steven <steven.liekens@gmail.com>
2022-05-27 13:34:05 +0000

    Remove unreliable devcontainer init script

--- .devcontainer/initialize-host.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# This script is executed on the host before the dev container is created.
-# Useful for just-in-time config.
-#
-
-if [ -d /run/WSL ]; then
-  git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
-fi
