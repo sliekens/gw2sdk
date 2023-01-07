d2bb7540980df9045f663fd5689ecbba9f960b62
Steven <steven.liekens@gmail.com>
2023-01-07 16:16:48 +0100

    Add api key to helper script

--- Invoke-Api.ps1
+++ Invoke-Api.ps1
@@ -1,9 +1,13 @@
 $endpoint = $args[0]
+
+$secrets = dotnet user-secrets list --json --project $PSScriptRoot/GW2SDK.Tests | ConvertFrom-Json
 $script = New-TemporaryFile
 
 Set-Content -Path $script -Value @(
     "connect https://api.guildwars2.com",
+    # "echo on"
     "set header X-Schema-Version 3",
+    "set header Authorization `"Bearer $($secrets.ApiKey)`""
     "get $endpoint"
 )
 
