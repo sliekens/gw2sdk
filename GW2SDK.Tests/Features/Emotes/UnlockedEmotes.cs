16d8b53dbc5bde7f371d472f52e1eb34ecde49d1
Steven <steven.liekens@gmail.com>
2023-01-30 00:51:09 +0100

    Test for unknown emotes

--- GW2SDK.Tests/Features/Emotes/UnlockedEmotes.cs
+++ GW2SDK.Tests/Features/Emotes/UnlockedEmotes.cs
@@ -14,6 +14,18 @@ public class UnlockedEmotes
 
         var actual = await sut.Emotes.GetUnlockedEmotes(accessToken.Key);
 
-        Assert.NotEmpty(actual.Value);
+        // Can be empty if you haven't unlocked any emotes
+        // The best we can do is verify that there are no unexpected emotes
+        Assert.All(actual.Value, chest => Assert.Contains(chest, new[]
+        {
+            "Shiverplus",
+            "Stretch",
+            "geargrind",
+            "playdead",
+            "rockout",
+            "shiver",
+            "shuffle",
+            "step"
+        }));
     }
 }
