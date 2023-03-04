5ef845c668dacec344914cc317251c7a9493e4e8
Steven <steven.liekens@gmail.com>
2023-03-04 14:13:47 +0100

    Add missing item restrictions

--- GW2SDK/Definitions/Enums/ItemRestriction.cs
+++ GW2SDK/Definitions/Enums/ItemRestriction.cs
@@ -2,24 +2,51 @@
 
 namespace GuildWars2;
 
+/// <summary>A list of possible restrictions for items that are limited to some races, genders or professions.</summary>
 [PublicAPI]
 public enum ItemRestriction
 {
+    #region Races
+
     Asura = 1,
 
     Charr,
 
+    Human,
+
+    Norn,
+
+    Sylvari,
+
+    #endregion Races
+
+    #region Genders
+
     Female,
 
-    Human,
+    Male,
+
+    #endregion Genders
+
+    #region Professions
+
+    Guardian = 1,
+
+    Warrior,
+
+    Engineer,
+
+    Ranger,
+
+    Thief,
+
+    Elementalist,
 
     Mesmer,
 
     Necromancer,
 
-    Norn,
+    Revenant
 
-    Ranger,
-
-    Sylvari
+    #endregion Professions
 }
