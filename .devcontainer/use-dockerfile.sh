8143a4f6c062c12bd803d0c4662177a62ae4f794
Steven Liekens <steven.liekens@gmail.com>
2023-05-21 00:23:51 +0000

    Improve how dev container is built
    The devcontainer.json is now configured to build an image from the Dockerfile instead of pulling a pre-built image. Importantly, the pre-built image is still used as a cache. So while pulling the image can still take a while as before, building the source Dockerfile is now super fast.
    
    This approach enables testing changes to the Dockerfile without making changes to devcontainer.json.

--- .devcontainer/use-dockerfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# This script reconfigures devcontainer.json to build a new image.
-#
-# Syntax: ./use-dockerfile
-devcontainer=$(dirname $(realpath $0))
-jq 'del(.image) + { build: { dockerfile: "Dockerfile" } }' $devcontainer/devcontainer.json > $devcontainer/devcontainer.json.tmp
-mv $devcontainer/devcontainer.json.tmp $devcontainer/devcontainer.json
