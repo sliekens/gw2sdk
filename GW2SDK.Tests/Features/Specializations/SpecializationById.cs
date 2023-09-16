01fb12f5d83abbde8870e0338d0506daef117a3d
Steven Liekens <steven.liekens@gmail.com>
2023-09-16 02:10:40 +0000

    Reorganize Specializations tests

---
+++ GW2SDK.Tests/Features/Specializations/SpecializationById.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using GuildWars2.Tests.TestInfrastructure;
+using Xunit;
+
+namespace GuildWars2.Tests.Features.Specializations;
+
+public class SpecializationById
+{
+    [Fact]
+    public async Task Can_be_found()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+
+        const int id = 1;
+
+        var actual = await sut.Specializations.GetSpecializationById(id);
+
+        Assert.Equal(id, actual.Value.Id);
+    }
+}
---
+++ GW2SDK.Tests/Features/Specializations/Specializations.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using GuildWars2.Tests.TestInfrastructure;
+using Xunit;
+
+namespace GuildWars2.Tests.Features.Specializations;
+
+public class Specializations
+{
+    [Fact]
+    public async Task Specializations_can_be_listed()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+
+        var actual = await sut.Specializations.GetSpecializations();
+
+        Assert.Equal(actual.ResultContext.ResultTotal, actual.Value.Count);
+        Assert.All(
+            actual.Value,
+            specialization =>
+            {
+                specialization.Id_is_positive();
+                specialization.Name_is_not_empty();
+                specialization.It_has_minor_traits();
+                specialization.It_has_major_traits();
+                specialization.Icon_is_not_empty();
+                specialization.Background_is_not_empty();
+                specialization.Big_profession_icon_is_not_null();
+                specialization.Profession_icon_is_not_null();
+            }
+        );
+    }
+}
