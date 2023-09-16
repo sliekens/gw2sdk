9bb96d89e113d8f767a281d8e20a95dc3f3aa02e
Steven Liekens <steven.liekens@gmail.com>
2023-09-16 02:15:29 +0000

    Reorganize Token tests

--- GW2SDK.Tests/Features/Tokens/SubtokenInfo.cs
+++ GW2SDK.Tests/Features/Tokens/SubtokenInfo.cs
@@ -8,33 +8,10 @@ using Xunit;
 
 namespace GuildWars2.Tests.Features.Tokens;
 
-public class TokenProviderTest
+public class SubtokenInfo
 {
     [Fact]
-    public async Task It_can_get_the_token_info_for_an_api_key()
-    {
-        var sut = Composer.Resolve<Gw2Client>();
-        var accessToken = Composer.Resolve<ApiKey>();
-
-        var actual = await sut.TokenProvider.GetTokenInfo(accessToken.Key);
-
-        var apiKey = Assert.IsType<ApiKeyInfo>(actual.Value);
-
-        Assert.NotEmpty(apiKey.Id);
-
-        // Your API key must be named GW2SDK-Full to pass this test
-        // This is not intended to improve account security, only to prevent key abuse
-        // The reason is that some services like GW2BLTC.com associate keys with logins but require you to use a key name of their choice
-        // If this key leaks to the outside world, it can't be (ab)used to login with GW2BLTC.com or similar sites
-        Assert.Equal("GW2SDK-Full", apiKey.Name);
-
-        var expectedPermissions = Enum.GetValues(typeof(Permission)).Cast<Permission>().ToHashSet();
-
-        Assert.Equal(expectedPermissions, apiKey.Permissions.ToHashSet());
-    }
-
-    [Fact]
-    public async Task It_can_get_the_token_info_for_a_subtoken()
+    public async Task Subtoken_has_info()
     {
         var sut = Composer.Resolve<Gw2Client>();
         var accessToken = Composer.Resolve<ApiKey>();
@@ -71,7 +48,7 @@ public class TokenProviderTest
 
         var actual = await sut.TokenProvider.GetTokenInfo(createdSubtoken.Value.Subtoken);
 
-        var subtoken = Assert.IsType<SubtokenInfo>(actual.Value);
+        var subtoken = Assert.IsType<GuildWars2.Tokens.SubtokenInfo>(actual.Value);
 
         Assert.NotEmpty(subtoken.Id);
 
---
+++ GW2SDK.Tests/Features/Tokens/TokenInfo.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using GuildWars2.Tests.TestInfrastructure;
+using GuildWars2.Tokens;
+using Xunit;
+
+namespace GuildWars2.Tests.Features.Tokens;
+
+public class TokenInfo
+{
+    [Fact]
+    public async Task Token_has_info()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+        var accessToken = Composer.Resolve<ApiKey>();
+
+        var actual = await sut.TokenProvider.GetTokenInfo(accessToken.Key);
+
+        var apiKey = Assert.IsType<ApiKeyInfo>(actual.Value);
+
+        Assert.NotEmpty(apiKey.Id);
+
+        // Your API key must be named GW2SDK-Full to pass this test
+        // This is not intended to improve account security, only to prevent key abuse
+        // The reason is that some services like GW2BLTC.com associate keys with logins but require you to use a key name of their choice
+        // If this key leaks to the outside world, it can't be (ab)used to login with GW2BLTC.com or similar sites
+        Assert.Equal("GW2SDK-Full", apiKey.Name);
+
+        var expectedPermissions = Enum.GetValues(typeof(Permission)).Cast<Permission>().ToHashSet();
+
+        Assert.Equal(expectedPermissions, apiKey.Permissions.ToHashSet());
+    }
+}
