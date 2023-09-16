c4f332543d702ae9da06825a5cf3db1f1dd1e7ce
Steven Liekens <steven.liekens@gmail.com>
2023-09-16 11:04:00 +0000

    Reorganize WorldBosses tests

---
+++ GW2SDK.Tests/Features/WorldBosses/WorldBosses.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using GuildWars2.Tests.TestInfrastructure;
+using Xunit;
+
+namespace GuildWars2.Tests.Features.WorldBosses;
+
+public class WorldBosses
+{
+    [Fact]
+    public async Task Can_be_listed()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+
+        var actual = await sut.WorldBosses.GetWorldBosses();
+        
+        Assert.NotEmpty(actual.Value);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultCount);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultTotal);
+    }
+}
