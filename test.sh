ced022d1a5be91d0bdaab8058995b9798fdfae03
Steven Liekens <steven.liekens@gmail.com>
2023-09-30 11:58:29 +0000

    Refresh Coverlet settings
    - Support deterministic build reports
    - Exclude vendor source code from coverage
    - Add ReportGenerator
    - Add test.sh helper script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Remove results from previous runs
+rm -r GW2SDK.Tests/TestResults/* || true
+
+# Run tests with coverage, additional arguments are passed on to dotnet test
+# e.g. ./test.sh --filter Colors
+# e.g. ./test.sh --framework net7.0
+dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings "$@"
+
+# Remove reports from previous runs
+rm -r reports
+
+# Generate coverage report
+dotnet reportgenerator -reports:*/TestResults/*/coverage.cobertura.xml -targetdir:reports
