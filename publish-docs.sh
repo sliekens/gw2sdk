e7baa1ecbf4d2154fba464112eaf79f17a7120c0
Steven Liekens <steven.liekens@gmail.com>
2023-10-07 13:56:46 +0000

    Update VuePress

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Build the documentation
 npm ci
 npm run build
+
+# Create a stash with the created documentation files
 git stash push --all -- docs/.vuepress/dist
+
+# Switch to the gh-pages branch
 git clean -xdf
 git fetch origin gh-pages
 git checkout gh-pages
+
+# Overwrite all files with the ones from the stash
 git rm -rf .
 git stash pop
 git add .
