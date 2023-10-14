715b6826dbff5510650735486bb81bb51a990908
Steven Liekens <steven.liekens@gmail.com>
2023-10-14 19:51:17 +0000

    Include HttpStatusCodeEx in all targets, just make obsolete

--- GW2SDK/Http/HttpStatusCodeEx.cs
+++ GW2SDK/Http/HttpStatusCodeEx.cs
@@ -1,12 +1,15 @@
-﻿#if !NET
-using System.Net;
+﻿using System.Net;
 
 namespace GuildWars2.Http;
 
+#if NET
+[Obsolete("Use System.Net.HttpStatusCode instead.")]
+#endif
 [PublicAPI]
+/// <summary>Provides additional HTTP status codes which are missing in older .NET versions.</summary>
 public static class HttpStatusCodeEx
 {
+    /// <summary>Equivalent to HTTP status 429. TooManyRequests indicates that the user has sent too many requests in a given amount of time.</summary>
     public const HttpStatusCode TooManyRequests = (HttpStatusCode)429;
 }
 
-#endif
