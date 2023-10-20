3670d258ccee1b1e53bbc37e778337f846503721
Steven Liekens <steven.liekens@gmail.com>
2023-10-21 00:25:24 +0200

    Fix XML doc comment placement

--- GW2SDK/Http/HttpStatusCodeEx.cs
+++ GW2SDK/Http/HttpStatusCodeEx.cs
@@ -2,12 +2,11 @@
 
 namespace GuildWars2.Http;
 
+/// <summary>Provides additional HTTP status codes which are missing in older .NET versions.</summary>
 #if NET
 [Obsolete("Use System.Net.HttpStatusCode instead.")]
 #endif
 [PublicAPI]
-
-/// <summary>Provides additional HTTP status codes which are missing in older .NET versions.</summary>
 public static class HttpStatusCodeEx
 {
     /// <summary>Equivalent to HTTP status 429. TooManyRequests indicates that the user has sent too many requests in a given
