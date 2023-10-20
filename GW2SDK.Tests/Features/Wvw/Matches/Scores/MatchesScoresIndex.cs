eddf5ac0d788205d905f2de1812a62c5a16d44dc
Steven Liekens <steven.liekens@gmail.com>
2023-10-21 01:35:41 +0200

    Add missing WvW tests

---
+++ GW2SDK.Tests/Features/Wvw/Matches/Scores/MatchesScoresIndex.cs
@@ -0,0 +1,18 @@
+﻿using GuildWars2.Tests.TestInfrastructure;
+
+namespace GuildWars2.Tests.Features.Wvw.Matches.Scores;
+
+public class MatchesScoresIndex
+{
+    [Fact]
+    public async Task Can_be_listed()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+
+        var actual = await sut.Wvw.GetMatchesScoresIndex();
+
+        Assert.NotEmpty(actual.Value);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultCount);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultTotal);
+    }
+}
---
+++ GW2SDK.Tests/Features/Wvw/Matches/Stats/MatchesStatsIndex.cs
@@ -0,0 +1,18 @@
+﻿using GuildWars2.Tests.TestInfrastructure;
+
+namespace GuildWars2.Tests.Features.Wvw.Matches.Stats;
+
+public class MatchesStatsIndex
+{
+    [Fact]
+    public async Task Can_be_listed()
+    {
+        var sut = Composer.Resolve<Gw2Client>();
+
+        var actual = await sut.Wvw.GetMatchesStatsIndex();
+
+        Assert.NotEmpty(actual.Value);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultCount);
+        Assert.Equal(actual.Value.Count, actual.ResultContext.ResultTotal);
+    }
+}
