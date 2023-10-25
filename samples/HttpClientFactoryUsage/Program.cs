2b998bd836574a2fda16a7c578189c1dc4fb6574
Steven Liekens <steven.liekens@gmail.com>
2023-10-25 20:10:11 +0200

    Revise HttpClientFactoryUsage sample

--- samples/HttpClientFactoryUsage/Program.cs
+++ samples/HttpClientFactoryUsage/Program.cs
@@ -1,38 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
 using GuildWars2;
-using GuildWars2.Achievements;
-using GuildWars2.Achievements.Dailies;
+using GuildWars2.Colors;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Pastel;
 
-var host = new HostBuilder().ConfigureServices(
-        services =>
-        {
-            services.AddHttpClient<Gw2Client>(
-                httpClient =>
-                {
-                    // Here you can further configure the HttpClient
-                    // e.g. you can set a different base address and a different timeout
-                    httpClient.BaseAddress = BaseAddress.DefaultUri;
-                    httpClient.Timeout = TimeSpan.FromSeconds(100);
-                }
-            );
-        }
-    )
-    .Build();
+var host = Host.CreateApplicationBuilder(args);
 
-var gw2 = host.Services.GetRequiredService<Gw2Client>();
-DailyAchievementGroup dailies = await gw2.Achievements.GetDailyAchievements();
-HashSet<Achievement> dailyFractals =
-    await gw2.Achievements.GetAchievementsByIds(
-        dailies.Fractals.Select(fractal => fractal.Id).ToList()
-    );
+// Use AddHttpClient to register the Gw2Client with the HttpClientFactory
+host.Services.AddHttpClient<Gw2Client>(
+    httpClient =>
+    {
+        // Here you can further configure the HttpClient
+        // e.g. you can set a different base address and a different timeout
+        httpClient.BaseAddress = BaseAddress.DefaultUri;
+        httpClient.Timeout = TimeSpan.FromSeconds(100);
+    }
+);
 
-Console.WriteLine("Daily fractals");
-Console.WriteLine("========================================");
-foreach (var fractal in dailyFractals)
+var app = host.Build();
+
+var gw2 = app.Services.GetRequiredService<Gw2Client>();
+
+var colors = await gw2.Dyes.GetColors();
+
+foreach (var dye in colors.Value)
 {
-    Console.WriteLine("{0,-40}: {1}", fractal.Name, fractal.Requirement);
+    PrintRow(dye.Name, dye.Cloth.Rgb, dye.Leather.Rgb, dye.Metal.Rgb);
+}
+
+void PrintRow(string name, Color cloth, Color leather, Color metal)
+{
+    app.Services.GetRequiredService<ILogger<Dye>>()
+        .LogInformation(
+            $"{name,-30} {"     ".Pastel(cloth).PastelBg(cloth),-25} {"     ".Pastel(leather).PastelBg(leather),-25} {"     ".Pastel(metal).PastelBg(metal),-25}"
+        );
 }
