d5eee3d2c0bc1d4cf6530f0fc5a1b195963a0981
Steven Liekens <steven.liekens@gmail.com>
2023-10-25 09:55:07 +0000

    Undo undo undo

--- samples/Mumble/Program.cs
+++ samples/Mumble/Program.cs
@@ -1,21 +1,159 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using GuildWars2;
+using GuildWars2.Mumble;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+// Using Microsoft.Extensions.Hosting is optional,
+// but it makes it easier to configure logging, dependency injection and cancellation
+var app = CreateApplication();
+var stoppingToken = app.GetStoppingToken();
+
+var logger = app.GetService<ILogger<GameLink>>();
 
 if (!GameLink.IsSupported())
 {
     throw new PlatformNotSupportedException();
 }
 
-var refreshInterval = TimeSpan.FromSeconds(1);
-using var gameLink = GameLink.Open(refreshInterval);
+// Prepare a dictionary of MapSummary and Specialization for later use
+var gw2 = app.GetService<Gw2Client>();
 
+var maps = (await gw2.Maps.GetMapSummaries(cancellationToken: stoppingToken))
+    .Value.ToDictionary(map => map.Id);
+
+var specializations = (await gw2.Specializations.GetSpecializations(cancellationToken: stoppingToken))
+    .Value.ToDictionary(specialization => specialization.Id);
+
+// Initialize the shared memory link
+var refreshInterval = TimeSpan.FromSeconds(1);
+var gameLink = GameLink.Open(refreshInterval);
+
+// Subscribe to the GameLink observable
+// I recommend using Rx (System.Reactive) instead of implementing IObserver<T> yourself
 gameLink.Subscribe(
-    gameTick =>
+
+    // OnNext(GameTick) is executed whenever the game client updates the shared memory
+    tick =>
     {
-        var player = gameTick.GetIdentity();
-        if (player is not null)
+        var identity = tick.GetIdentity();
+        if (identity is null)
         {
-            Console.WriteLine($"{player.Name} is ready to go!");
+            // Identity might be null due to invalid JSON
+            // (happens very rarely due to concurrent reads/writes)
+            return;
         }
-    }
+
+        var specialization = "no specialization";
+        if (specializations.TryGetValue(identity.SpecializationId, out var found))
+        {
+            specialization = found.Name;
+        }
+
+        var map = maps[identity.MapId];
+
+        var title = $"the {identity.Race} {identity.Profession}";
+        if (!tick.Context.UiState.HasFlag(UiState.GameHasFocus))
+        {
+            logger.LogInformation(
+                "[{UiTick}] {Name}, {Title} ({Specialization}) is afk",
+                tick.UiTick,
+                identity.Name,
+                title,
+                specialization
+            );
+        }
+        else if (tick.Context.UiState.HasFlag(UiState.TextboxHasFocus))
+        {
+            logger.LogInformation(
+                "[{UiTick}] {Name}, {Title} ({Specialization}) is typing",
+                tick.UiTick,
+                identity.Name,
+                title,
+                specialization
+            );
+        }
+        else if (tick.Context.UiState.HasFlag(UiState.IsMapOpen))
+        {
+            logger.LogInformation(
+                "[{UiTick}] {Name}, {Title} ({Specialization}) is looking at the map",
+                tick.UiTick,
+                identity.Name,
+                title,
+                specialization
+            );
+        }
+        else if (tick.Context.UiState.HasFlag(UiState.IsInCombat))
+        {
+            logger.LogInformation(
+                "[{UiTick}] {Name}, {Title} ({Specialization}) is in combat",
+                tick.UiTick,
+                identity.Name,
+                title,
+                specialization
+            );
+        }
+        else
+        {
+            var transport = tick.Context.IsMounted ? tick.Context.Mount.ToString() : "foot";
+            logger.LogInformation(
+                "[{UiTick}] {Name}, {Title} ({Specialization}) is on {Transport} in {Map} ({Type}), Position: {{ Latitude = {X}, Longitude = {Z}, Elevation = {Y} }}",
+                tick.UiTick,
+                identity.Name,
+                title,
+                specialization,
+                transport,
+                map.Name,
+                tick.Context.MapType.ToString(),
+                tick.AvatarPosition.X,
+                tick.AvatarPosition.Z,
+                tick.AvatarPosition.Y
+            );
+        }
+    },
+
+    // OnError(Exception) is executed when there is an internal error while reading from the shared memory
+    // or when your OnNext callback throws an exception, it will also end up here
+    err =>
+    {
+        logger.LogError(err, "Something went wrong.");
+    },
+
+    // OnComplete callback runs when you unsubscribe or when the GameLink is being disposed
+    () =>
+    {
+        logger.LogInformation("Stopping on tick {UiTick}", gameLink.GetSnapshot().UiTick);
+    },
+    stoppingToken
 );
+
+app.Run();
+
+IHost CreateApplication()
+{
+    var host = Host.CreateApplicationBuilder(args);
+
+    // Configure services
+    host.Services.AddHttpClient<Gw2Client>();
+
+    // Configure logging
+    host.Logging.AddSimpleConsole(
+        options =>
+        {
+            options.SingleLine = true;
+            options.TimestampFormat = "HH:mm:ss.fff ";
+            options.UseUtcTimestamp = true;
+        }
+    );
+
+    return host.Build();
+}
+
+public static class HostExtensions
+{
+    public static T GetService<T>(this IHost app) where T : notnull => app.Services.GetRequiredService<T>();
+    public static CancellationToken GetStoppingToken(this IHost app) => app.GetService<IHostApplicationLifetime>().ApplicationStopping;
+}
