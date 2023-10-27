d357bffe6e5d95f9c1c3a22c10bed314db695ec3
Steven Liekens <steven.liekens@gmail.com>
2023-10-27 21:16:38 +0200

    Fix resilience handler in tests, stop using ServiceCollection

--- GW2SDK.Tests/TestInfrastructure/Composer.cs
+++ GW2SDK.Tests/TestInfrastructure/Composer.cs
@@ -2,13 +2,6 @@
 
 public static class Composer
 {
-    private static readonly IHttpClientFactory HttpClientFactory;
-
-    static Composer()
-    {
-        HttpClientFactory = new TestHttpClientFactory(BaseAddress.DefaultUri);
-    }
-
     public static T Resolve<T>() =>
         (T)GetService(typeof(T))
         ?? throw new InvalidOperationException($"Unable to compose type '{typeof(T)}'");
@@ -42,13 +35,41 @@ public static class Composer
 
         if (serviceType == typeof(Gw2Client))
         {
-            var httpClient = HttpClientFactory.CreateClient("GW2SDK");
-            return new Gw2Client(httpClient);
+            return new Gw2Client(Resolve<HttpClient>());
         }
 
         if (serviceType == typeof(HttpClient))
         {
-            return HttpClientFactory.CreateClient("GW2SDK");
+            return new HttpClient(
+                new SchemaVersionHandler
+                {
+                    InnerHandler = new ResilienceHandler(
+#if NET
+                        new SocketsHttpHandler
+                        {
+                            // Limit the number of open connections
+                            //   because we have many tests trying to use the API concurrently,
+                            //   resulting in a stupid amount of connections being opened
+                            // The desired effect is to open a smaller number of connections that are reused often
+                            MaxConnectionsPerServer = 20,
+
+                            // Creating a new connection shouldn't take more than 10 seconds
+                            ConnectTimeout = TimeSpan.FromSeconds(10),
+                            PooledConnectionLifetime = TimeSpan.FromMinutes(5)
+                        }
+#else
+                        new HttpClientHandler { MaxConnectionsPerServer = 20 }
+#endif
+                    )
+                }
+            )
+            {
+                // The default timeout is 100 seconds, but it's not always enough
+                // Requests can get stuck in a delayed retry-loop due to rate limiting
+                // A better solution might be to queue up requests
+                //   (so that new requests have to wait until there are no more delayed requests)
+                Timeout = TimeSpan.FromMinutes(5)
+            };
         }
 
         return null;
---
+++ GW2SDK.Tests/TestInfrastructure/ResilienceHandler.cs
@@ -0,0 +1,123 @@
+﻿using System.Text.Json;
+using Polly;
+using Polly.Hedging;
+using Polly.Retry;
+using static System.Net.HttpStatusCode;
+
+#if NETFRAMEWORK
+using static GuildWars2.Http.HttpStatusCodeEx;
+#endif
+
+namespace GuildWars2.Tests.TestInfrastructure
+{
+    public class ResilienceHandler : DelegatingHandler
+    {
+        private readonly ResiliencePipeline<HttpResponseMessage> resiliencePipeline;
+
+        public ResilienceHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            resiliencePipeline = new ResiliencePipelineBuilder<HttpResponseMessage>().AddRetry(
+                    new RetryStrategyOptions<HttpResponseMessage>
+                    {
+                        ShouldHandle = async attempt => attempt.Outcome switch
+                        {
+                            // Retry on too many requests
+                            { Result.StatusCode: TooManyRequests } => true,
+
+                            // Retry on Service Unavailable just once
+                            // because we don't know if it's intentional or due to technical difficulties
+                            { Result.StatusCode: ServiceUnavailable } => attempt.AttemptNumber == 0
+                                && await GetText(attempt.Outcome) != "API not active",
+
+                            _ => false
+                        },
+                        MaxRetryAttempts = 100,
+                        BackoffType = DelayBackoffType.Constant,
+                        Delay = TimeSpan.FromSeconds(30),
+                        UseJitter = true
+                    }
+                )
+                .AddHedging(
+                    new HedgingStrategyOptions<HttpResponseMessage>
+                    {
+                        // If no response is received within 30 seconds, abort the in-flight request and retry
+                        Delay = TimeSpan.FromSeconds(30),
+                        MaxHedgedAttempts = 10,
+                        ShouldHandle = async attempt =>
+                        {
+                            return attempt.Outcome switch
+                            {
+                                { Result.IsSuccessStatusCode: true } => false,
+
+                                // The following replies are considered retryable without a back-off delay
+                                {
+                                    Result.StatusCode: InternalServerError
+                                    or BadGateway
+                                    or GatewayTimeout
+                                } => true,
+
+                                // Sometimes the API returns weird data, also treat as internal errors
+                                _ => await GetText(attempt.Outcome) is
+                                    "endpoint requires authentication"
+                                    or "unknown error"
+                                    or "ErrBadData"
+                                    or "ErrTimeout"
+                            };
+                        },
+                    }
+                )
+                .Build();
+
+            static async Task<string> GetText(Outcome<HttpResponseMessage> attempt)
+            {
+                if (attempt.Result is null)
+                {
+                    return null;
+                }
+
+                if (attempt.Result.Content.Headers.ContentType?.MediaType != "application/json")
+                {
+                    return null;
+                }
+
+                // IMPORTANT: buffer the content so it can be read multiple times if needed
+                await attempt.Result.Content.LoadIntoBufferAsync();
+
+                // ALSO IMPORTANT: do not dispose the content stream
+                var content = await attempt.Result.Content.ReadAsStreamAsync();
+                try
+                {
+                    using var json = await JsonDocument.ParseAsync(content);
+                    if (!json.RootElement.TryGetProperty("text", out var text))
+                    {
+                        return null;
+                    }
+
+                    // Sometimes you get an authentication error even though your API key is valid
+                    // Treat this message as an internal error, because you get a different message if the API key is really invalid
+                    return text.GetString();
+                }
+                finally
+                {
+                    content.Position = 0;
+                }
+            }
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken
+        )
+        {
+            return await resiliencePipeline.ExecuteAsync(
+                    async ct =>
+                    {
+                        return await base.SendAsync(request, ct).ConfigureAwait(false);
+                    },
+                    cancellationToken
+                )
+                .ConfigureAwait(false);
+        }
+    }
+}
