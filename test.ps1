caa5b1e317785d9147b04ab80775ce6457baa805
Steven Liekens <steven.liekens@gmail.com>
2023-10-28 01:27:12 +0200

    Add reportgenerator helper for pwsh

---
+++ test.ps1
@@ -0,0 +1,13 @@
+# Remove results from previous runs
+Remove-Item -ErrorAction Ignore -Recurse GW2SDK.Tests/TestResults/*
+
+# Run tests with coverage, additional arguments are passed on to dotnet test
+# e.g. ./test.sh --filter Colors
+# e.g. ./test.sh --framework net8.0
+dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings $args
+
+# Remove reports from previous runs
+Remove-Item -ErrorAction Ignore -Recurse reports
+
+# Generate coverage report
+dotnet reportgenerator -reports:*/TestResults/*/coverage.cobertura.xml -targetdir:reports
