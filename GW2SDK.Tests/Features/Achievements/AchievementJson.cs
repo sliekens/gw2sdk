363424e3c93940765f3b626fb79db3f19d23352b
Steven Liekens <steven.liekens@gmail.com>
2023-10-28 13:32:53 +0200

    Use Assert.All in tests with cached test data

---
+++ GW2SDK.Tests/Features/Achievements/AchievementJson.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using GuildWars2.Achievements;
+
+namespace GuildWars2.Tests.Features.Achievements;
+
+public class AchievementJson : IClassFixture<AchievementFixture>
+{
+    public AchievementJson(AchievementFixture fixture)
+    {
+        this.fixture = fixture;
+    }
+
+    private readonly AchievementFixture fixture;
+
+    [Fact]
+    public void Achievements_can_be_created_from_json() =>
+        Assert.All(
+            fixture.Achievements,
+            json =>
+            {
+                using var document = JsonDocument.Parse(json);
+
+                var actual = document.RootElement.GetAchievement(MissingMemberBehavior.Error);
+
+                actual.Has_name();
+                actual.Has_description();
+                actual.Has_requirement();
+                actual.Has_LockedText();
+                actual.Has_flags();
+                actual.Has_tiers();
+                actual.Tiers_does_not_contain_null();
+                actual.Rewards_does_not_contain_null();
+                actual.Bits_does_not_contain_null();
+                actual.PointCap_is_negative_1_for_repeatable_achievements_without_points();
+            }
+        );
+}
--- GW2SDK.Tests/Features/Skins/SkinJson.cs
+++ GW2SDK.Tests/Features/Skins/SkinJson.cs
@@ -12,17 +12,17 @@ public class SkinJson : IClassFixture<SkinFixture>
 
     private readonly SkinFixture fixture;
 
-
     [Fact]
-    public void Skins_can_be_created_from_json()
-    {
-        foreach (var json in fixture.Skins)
-        {
-            using var document = JsonDocument.Parse(json);
+    public void Skins_can_be_created_from_json() =>
+        Assert.All(
+            fixture.Skins,
+            json =>
+            {
+                using var document = JsonDocument.Parse(json);
 
-            var actual = document.RootElement.GetSkin(MissingMemberBehavior.Error);
+                var actual = document.RootElement.GetSkin(MissingMemberBehavior.Error);
 
-            actual.Has_id();
-        }
-    }
+                actual.Has_id();
+            }
+        );
 }
