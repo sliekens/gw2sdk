bedc4e1d9402b64c352079dcacfacf594e26ad81
Steven Liekens <steven.liekens@gmail.com>
2023-11-05 13:56:00 +0100

    Start working on HeroClient

---
+++ GW2SDK/Features/Hero/HeroClient.cs
@@ -0,0 +1,16 @@
+﻿namespace GuildWars2.Hero;
+
+/// <summary>Provides query methods for things you can find in the Hero panel.</summary>
+[PublicAPI]
+public sealed class HeroClient
+{
+    private readonly HttpClient httpClient;
+
+    public HeroClient(HttpClient httpClient)
+    {
+        this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        this.httpClient.BaseAddress ??= BaseAddress.DefaultUri;
+    }
+
+
+}
