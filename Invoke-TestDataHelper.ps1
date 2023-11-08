bb285d5206dccaf5ac42232b9293e0f58852aad2
Steven Liekens <steven.liekens@gmail.com>
2023-11-08 01:11:56 +0100

    Fix test data utility

--- Invoke-TestDataHelper.ps1
+++ Invoke-TestDataHelper.ps1
@@ -1,2 +1,2 @@
 #!/usr/bin/env pwsh
-dotnet run -c Release --project .\GW2SDK.TestDataHelper\GW2SDK.TestDataHelper.csproj --out-dir:"$PSScriptRoot/GW2SDK.Tests/Data"
+dotnet run -c Release --project .\GW2SDK.TestDataHelper\GW2SDK.TestDataHelper.csproj -- "$PSScriptRoot/GW2SDK.Tests/Data"
