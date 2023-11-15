346756f897422e30edb62228f152c51e44533bde
Steven Liekens <steven.liekens@gmail.com>
2023-11-15 01:04:19 +0100

    Add World doc comments

--- GW2SDK/Features/Worlds/World.cs
+++ GW2SDK/Features/Worlds/World.cs
@@ -1,12 +1,19 @@
 ﻿namespace GuildWars2.Worlds;
 
+/// <summary>Information about a world (AKA server).</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record World
 {
+    /// <summary>The world ID.</summary>
     public required int Id { get; init; }
 
+    /// <summary>The name of the server.</summary>
     public required string Name { get; init; }
 
+    /// <summary>The population of the server. Transferring to a different world is only possible when it is not
+    /// <see cref="WorldPopulation.Full" />. The transfer cost will be calculated based on the population: 500 gems for
+    /// <see cref="WorldPopulation.Medium" />, 1000 gems for <see cref="WorldPopulation.High" /> and 1800 gems for
+    /// <see cref="WorldPopulation.VeryHigh" />.</summary>
     public required WorldPopulation Population { get; init; }
 }
--- GW2SDK/Features/Worlds/WorldsClient.cs
+++ GW2SDK/Features/Worlds/WorldsClient.cs
@@ -2,7 +2,7 @@
 
 namespace GuildWars2.Worlds;
 
-/// <summary>Provides query methods for World (servers) names and population.</summary>
+/// <summary>Provides query methods for World (servers).</summary>
 [PublicAPI]
 public sealed class WorldsClient
 {
@@ -16,6 +16,11 @@ public sealed class WorldsClient
 
     #region v2/worlds
 
+    /// <summary>Retrieves all worlds.</summary>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<World> Value, MessageContext Context)> GetWorlds(
         Language? language = default,
         MissingMemberBehavior missingMemberBehavior = default,
@@ -30,6 +35,9 @@ public sealed class WorldsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves the IDs of all worlds.</summary>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<int> Value, MessageContext Context)> GetWorldsIndex(
         CancellationToken cancellationToken = default
     )
@@ -38,6 +46,12 @@ public sealed class WorldsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a world by its ID.</summary>
+    /// <param name="worldId">The world ID.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(World Value, MessageContext Context)> GetWorldById(
         int worldId,
         Language? language = default,
@@ -53,6 +67,12 @@ public sealed class WorldsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves worlds by their IDs.</summary>
+    /// <param name="worldIds">The world IDs.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<World> Value, MessageContext Context)> GetWorldsByIds(
         IReadOnlyCollection<int> worldIds,
         Language? language = default,
@@ -68,6 +88,13 @@ public sealed class WorldsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a page of worlds.</summary>
+    /// <param name="pageIndex">How many pages to skip. The first page starts at 0.</param>
+    /// <param name="pageSize">How many entries to take.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<World> Value, MessageContext Context)> GetWorldsByPage(
         int pageIndex,
         int? pageSize = default,
