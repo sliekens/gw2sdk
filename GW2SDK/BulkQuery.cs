5a383a8c7160b956c663fe8dc16e44b20d618786
Steven Liekens <steven.liekens@gmail.com>
2023-11-19 03:09:27 +0100

    Refactor BulkQuery, add comments

--- GW2SDK/BulkQuery.cs
+++ GW2SDK/BulkQuery.cs
@@ -3,27 +3,48 @@ using GuildWars2.Tasks;
 
 namespace GuildWars2;
 
+/// <summary>Provides a method to retrieve bulk data from the Guild Wars 2 API.</summary>
 [PublicAPI]
 public static class BulkQuery
 {
+    /// <summary>The default chunk size is 200. This is the maximum number of keys that can be retrieved in a single query.</summary>
     public const int DefaultChunkSize = 200;
 
+    /// <summary>The default degree of parallelism is 20. This is the maximum number of concurrent queries that will be
+    /// executed in a bulk.</summary>
     public const int DefaultDegreeOfParallelism = 20;
 
-    /// <summary>Helps you retrieve all records in a given index by splitting it into smaller chunks. This is useful when your
-    /// index contains more than 200 items (the maximum allowed per query).</summary>
-    public static async IAsyncEnumerable<TRecord> QueryAsync<TKey, TRecord>(
-        IReadOnlyCollection<TKey> index,
-        ChunkQuery<TKey, TRecord> chunkQuery,
+    /// <summary>Retrieves bulk data from the Guild Wars 2 API. <br /> Given a large collection of keys, this method will the
+    /// collection into smaller chunks and query the API for each chunk. The chunk size can optionally be specified, otherwise
+    /// the maximum chunk size (200) is used. <br /> Chunks are queried in parallel by default. The results are streamed as
+    /// they become available, ordered by completion. The degree of parallelism can optionally be specified (default: 20). You
+    /// can optionally specify a progress object to receive progress updates.</summary>
+    /// <typeparam name="TKey">The data type of the keys.</typeparam>
+    /// <typeparam name="TValue">The data type of the values.</typeparam>
+    /// <param name="keys">The keys for which to retrieve the values.</param>
+    /// <param name="bulkRequest">The method that takes a set of keys and retrieves the corresponding values. The method must
+    /// be thread-safe. The method must not return null, duplicate values, values that do not correspond to the keys. The
+    /// method may return values out of order or return fewer values than the number of keys.</param>
+    /// <param name="degreeOfParallelism">The maximum number of concurrent requests.</param>
+    /// <param name="chunkSize">The maximum number of keys to retrieve in a single request.</param>
+    /// <param name="progress">An optional progress object to receive progress updates: result count and result total.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>The collection of values found.</returns>
+    /// <exception cref="ArgumentException"> Thrown when the keys collection is empty. </exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="chunkSize" /> is less than 1 or greater than
+    /// 200, or when <paramref name="degreeOfParallelism" /> is less than 1.</exception>
+    public static async IAsyncEnumerable<TValue> QueryAsync<TKey, TValue>(
+        IReadOnlyCollection<TKey> keys,
+        BulkRequest<TKey, TValue> bulkRequest,
         int degreeOfParallelism = DefaultDegreeOfParallelism,
         int chunkSize = DefaultChunkSize,
         IProgress<ResultContext>? progress = default,
         [EnumeratorCancellation] CancellationToken cancellationToken = default
     )
     {
-        if (index.Count == 0)
+        if (keys.Count == 0)
         {
-            throw new ArgumentException("The index cannot be empty.", nameof(index));
+            throw new ArgumentException("The keys collection cannot be empty.", nameof(keys));
         }
 
         if (chunkSize is < 1 or > 200)
@@ -45,14 +66,14 @@ public static class BulkQuery
         }
 
         var resultCount = 0;
-        var resultTotal = index.Count;
+        var resultTotal = keys.Count;
 
         progress?.Report(new ResultContext(resultTotal, resultCount));
 
         // PERF: no need to create chunks if index does not exceed the chunk size
-        if (index.Count <= chunkSize)
+        if (keys.Count <= chunkSize)
         {
-            var result = await chunkQuery(index, cancellationToken).ConfigureAwait(false);
+            var result = await bulkRequest(keys, cancellationToken).ConfigureAwait(false);
             progress?.Report(new ResultContext(resultTotal, result.Count));
             foreach (var record in result)
             {
@@ -64,7 +85,7 @@ public static class BulkQuery
         }
 
         // Split the index into smaller chunks to avoid hitting the 200 item limit
-        var chunks = Chunk(index.ToList(), chunkSize);
+        var chunks = Chunk(keys.ToList(), chunkSize);
 
         // Proceed with the queries in parallel, but limit the number of concurrent queries
         using SemaphoreSlim limiter = new(degreeOfParallelism);
@@ -72,7 +93,7 @@ public static class BulkQuery
                 async chunk =>
                 {
                     await limiter.WaitAsync(cancellationToken).ConfigureAwait(false);
-                    return await chunkQuery(chunk, cancellationToken).ConfigureAwait(false);
+                    return await bulkRequest(chunk, cancellationToken).ConfigureAwait(false);
                 }
             )
             .ToList()
---
+++ GW2SDK/BulkRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace GuildWars2;
+
+/// <summary>A method that takes a set of keys and retrieves the corresponding values.</summary>
+/// <typeparam name="TKey">The data type of the key.</typeparam>
+/// <typeparam name="TValue">The data type of the values.</typeparam>
+/// <param name="keys">The keys for which to retrieve the values.</param>
+/// <param name="cancellationToken">A token to cancel the request.</param>
+/// <returns>The collection of values found.</returns>
+[PublicAPI]
+public delegate Task<IReadOnlyCollection<TValue>> BulkRequest<in TKey, TValue>(
+    IReadOnlyCollection<TKey> keys,
+    CancellationToken cancellationToken = default
+);
