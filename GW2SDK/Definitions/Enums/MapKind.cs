608b99301faa34a8851240e5a0d3390f398cee61
Steven Liekens <steven.liekens@gmail.com>
2023-12-03 14:54:01 +0100

    Add MapSummary comments

--- GW2SDK/Definitions/Enums/MapKind.cs
+++ GW2SDK/Definitions/Enums/MapKind.cs
@@ -2,29 +2,42 @@
 
 namespace GuildWars2;
 
+/// <summary>The kind of maps in Guild Wars 2.</summary>
 [PublicAPI]
 [DefaultValue(Unknown)]
 public enum MapKind
 {
+    /// <summary>An unknown map kind, probably only used for new maps.</summary>
     Unknown,
 
+    /// <summary>An open world map.</summary>
     Public,
 
+    /// <summary>An instance map. Instance maps are maps that are only accessible to a single party or squad. Examples include
+    /// dungeons, fractals, strike missions and raids.</summary>
     Instance,
 
+    /// <summary>A jump puzzle map.</summary>
     JumpPuzzle,
 
+    /// <summary>The starter zone of a new character.</summary>
     Tutorial,
 
+    /// <summary>A structured PvP map.</summary>
     Pvp,
 
+    /// <summary>(WvW) Eternal Battlegrounds.</summary>
     Center,
 
+    /// <summary>(WvW) Red Borderlands.</summary>
     RedHome,
 
+    /// <summary>(WvW) Blue Borderlands.</summary>
     BlueHome,
 
+    /// <summary>(WvW) Green Borderlands.</summary>
     GreenHome,
 
+    /// <summary>(WvW) Edge of the Mists.</summary>
     EdgeOfTheMists
 }
--- GW2SDK/Features/Exploration/Maps/MapSummary.cs
+++ GW2SDK/Features/Exploration/Maps/MapSummary.cs
@@ -2,34 +2,47 @@
 
 namespace GuildWars2.Exploration.Maps;
 
-/// <summary>Provides a short summary of a map.</summary>
+/// <summary>Information about a map.</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record MapSummary
 {
+    /// <summary>The map ID.</summary>
     public required int Id { get; init; }
 
+    /// <summary>The name of the map.</summary>
     public required string Name { get; init; }
 
+    /// <summary>The minimum player level of the map.</summary>
     public required int MinLevel { get; init; }
 
+    /// <summary>The maximum player level of the map.</summary>
     public required int MaxLevel { get; init; }
 
+    /// <summary>The default floor of the map.</summary>
     public required int DefaultFloor { get; init; }
 
+    /// <summary>The kind of map.</summary>
     public required MapKind Kind { get; init; }
 
+    /// <summary>The floors of the map.</summary>
     public required IReadOnlyCollection<int> Floors { get; init; }
 
+    /// <summary>The region ID, or null for certain instance maps.</summary>
     public required int? RegionId { get; init; }
 
+    /// <summary>The name of the region, or empty for certain instance maps.</summary>
     public required string RegionName { get; init; }
 
+    /// <summary>The continent ID, or null for certain instance maps.</summary>
     public required int? ContinentId { get; init; }
 
+    /// <summary>The name of the continent, or empty for certain instance maps.</summary>
     public required string ContinentName { get; init; }
 
+    /// <summary>The dimensions of the map.</summary>
     public required Rectangle MapRectangle { get; init; }
 
+    /// <summary>The dimensions of the map within the continent coordinate system.</summary>
     public required Rectangle ContinentRectangle { get; init; }
 }
