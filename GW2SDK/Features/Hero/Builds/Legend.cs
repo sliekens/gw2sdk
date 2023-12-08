37bfcce35fe8fab1e72804065aaceff706f10202
Steven Liekens <steven.liekens@gmail.com>
2023-12-08 22:57:05 +0100

    Add Legend doc comments

--- GW2SDK/Features/Hero/Builds/Legend.cs
+++ GW2SDK/Features/Hero/Builds/Legend.cs
@@ -1,18 +1,25 @@
 ﻿namespace GuildWars2.Hero.Builds;
 
+/// <summary>Information about a revenant legend.</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record Legend
 {
+    /// <summary>The legend ID.</summary>
     public required string Id { get; init; }
 
+    /// <summary>The legend numeric code, which is mainly used in chat links instead of the real <see cref="Id" />.</summary>
     public required int Code { get; init; }
 
+    /// <summary>The skill ID of the legend's swap skill.</summary>
     public required int Swap { get; init; }
 
+    /// <summary>The skill ID of the legend's healing skill.</summary>
     public required int Heal { get; init; }
 
+    /// <summary>The skill ID of the legend's elite skill.</summary>
     public required int Elite { get; init; }
 
+    /// <summary>The skill IDs of the legend's utility skills.</summary>
     public required IReadOnlyCollection<int> Utilities { get; init; }
 }
