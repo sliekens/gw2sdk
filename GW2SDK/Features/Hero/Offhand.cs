dcd0ab81db4c40ad812eb5535baa05a5b7fb31da
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 06:06:08 +0100

    Move Offhand to Hero

---
+++ GW2SDK/Features/Hero/Offhand.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace GuildWars2.Hero;
+
+/// <summary>The offhands which may be required for a weapon skill.</summary>
+[PublicAPI]
+[DefaultValue(None)]
+public enum Offhand
+{
+    /// <summary>Nothing in the offhand (empty).</summary>
+    None,
+
+    /// <summary>Nothing in the offhand (empty).</summary>
+    Nothing = None,
+
+    /// <summary>A dagger in the offhand.</summary>
+    Dagger,
+
+    /// <summary>A pistol in the offhand.</summary>
+    Pistol
+}
