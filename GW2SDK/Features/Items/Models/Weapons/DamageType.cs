9330b0eeb2da63d40239303bb0ab73a712166fd5
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 03:44:49 +0100

    Move DamageType to Items

---
+++ GW2SDK/Features/Items/Models/Weapons/DamageType.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace GuildWars2.Items;
+
+/// <summary>The type of damage dealt by a weapon. It is a purely visual effect and does not affect the damage calculation.</summary>
+[PublicAPI]
+[DefaultValue(Physical)]
+public enum DamageType
+{
+    /// <summary>The default damage type of most weapons, which does not have a special visual effect.</summary>
+    Physical,
+
+    /// <summary>Creatures that die from a Choking attack go through a suffocation animation.</summary>
+    Choking,
+
+    /// <summary>Creatures that die from a Fire attack will collapse in a flaming heap.</summary>
+    Fire,
+
+    /// <summary>Creatures that die from an Ice attack will become pale and stiff before collapsing.</summary>
+    Ice,
+
+    /// <summary>Creatures that die from a Lightning attack stiffen and shake uncontrollably, then collapse.</summary>
+    Lightning
+}
