2da7fe370e8fe1644c7bc20c540603c18a7f2252
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 03:38:05 +0100

    Move ComboFinisherName to Hero.Builds.Facts

---
+++ GW2SDK/Features/Hero/Builds/Facts/ComboFinisherName.cs
@@ -0,0 +1,19 @@
+﻿namespace GuildWars2.Hero.Builds.Facts;
+
+/// <summary>The combo finisher that is applied by a skill.</summary>
+[PublicAPI]
+public enum ComboFinisherName
+{
+    /// <summary>Blast finishers creates area effects.</summary>
+    Blast = 1,
+
+    /// <summary>Leap finishers apply buffs or cause conditions.</summary>
+    Leap,
+
+    /// <summary>Projectile finishers cause a condition to foes when the projectile hits, or apply an effect to an adjacent
+    /// ally.</summary>
+    Projectile,
+
+    /// <summary>Whirl finishers causes projectiles to be fired in multiple directions, applying effects to targets they hit.</summary>
+    Whirl
+}
