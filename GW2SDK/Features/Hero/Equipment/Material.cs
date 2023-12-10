403121a3fee10ea72292daa33c60ae55641bfb6c
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 05:31:15 +0100

    Move Material to Hero.Equipment

--- GW2SDK/Features/Hero/Equipment/Material.cs
+++ GW2SDK/Features/Hero/Equipment/Material.cs
@@ -1,13 +1,18 @@
-﻿namespace GuildWars2;
+﻿namespace GuildWars2.Hero.Equipment;
 
+/// <summary>The material of an armor piece, which determines its appearance.</summary>
 [PublicAPI]
 public enum Material
 {
+    /// <summary>Cloth armor.</summary>
     Cloth = 1,
 
+    /// <summary>Leather armor.</summary>
     Leather,
 
+    /// <summary>Metal armor.</summary>
     Metal,
 
+    /// <summary>Fur armor.</summary>
     Fur
 }
