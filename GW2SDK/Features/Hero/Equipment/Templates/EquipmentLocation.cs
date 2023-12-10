e9d606202a0d0f857296cd65b0c6bef20deae9a4
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 03:52:50 +0100

    Move EquipmentLocation to Hero.Equipment.Templates

---
+++ GW2SDK/Features/Hero/Equipment/Templates/EquipmentLocation.cs
@@ -0,0 +1,18 @@
+﻿namespace GuildWars2.Hero.Equipment.Templates;
+
+/// <summary>The location of an equipment item.</summary>
+[PublicAPI]
+public enum EquipmentLocation
+{
+    /// <summary>The item is equipped in the current active template.</summary>
+    Equipped = 1,
+
+    /// <summary>The item is equipped in an inactive template.</summary>
+    Armory,
+
+    /// <summary>The item is equipped in the current active template, and it is Legendary.</summary>
+    EquippedFromLegendaryArmory,
+
+    /// <summary>The item is equipped in an inactive template, and it is Legendary.</summary>
+    LegendaryArmory
+}
