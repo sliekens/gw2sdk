1d88b274b9c90354278ab746d0b0329ec5d6bfe4
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 04:02:10 +0100

    Move Gender to Hero.Accounts

--- GW2SDK/Definitions/Enums/Gender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GuildWars2;
-
-[PublicAPI]
-public enum Gender
-{
-    Female = 1,
-
-    Male
-}
---
+++ GW2SDK/Features/Hero/Accounts/Gender.cs
@@ -0,0 +1,12 @@
+﻿namespace GuildWars2.Hero.Accounts;
+
+/// <summary>The body types of characters.</summary>
+[PublicAPI]
+public enum Gender
+{
+    /// <summary>Female characters.</summary>
+    Female = 1,
+
+    /// <summary>Male characters</summary>
+    Male
+}
