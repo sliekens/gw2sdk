32cc53f95e6b7366d5222c5432e2bbb03381c221
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 02:44:31 +0100

    Move Attunement to Hero

---
+++ GW2SDK/Features/Hero/Attunement.cs
@@ -0,0 +1,18 @@
+﻿namespace GuildWars2.Hero;
+
+/// <summary>The attunement of an Elementalist.</summary>
+[PublicAPI]
+public enum Attunement
+{
+    /// <summary>Attunement to Earth.</summary>
+    Earth = 1,
+
+    /// <summary>Attunement to Water.</summary>
+    Water,
+
+    /// <summary>Attunement to Air.</summary>
+    Air,
+
+    /// <summary>Attunement to Fire.</summary>
+    Fire
+}
