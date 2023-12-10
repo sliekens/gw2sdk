c6a1a6a7f8d3b5bf5770171884b3e68d2af99112
Steven Liekens <steven.liekens@gmail.com>
2023-12-10 06:49:19 +0100

    Move Transformation to Hero.Builds

---
+++ GW2SDK/Features/Hero/Builds/Transformation.cs
@@ -0,0 +1,9 @@
+﻿namespace GuildWars2.Hero.Builds;
+
+/// <summary>Transformations related to specializations.</summary>
+[PublicAPI]
+public enum Transformation
+{
+    /// <summary>Druid's Celestial Avatar.</summary>
+    CelestialAvatar = 1
+}
