9411146a5f8f86b277c8fe3b409df68d17deb694
Steven Liekens <steven.liekens@gmail.com>
2023-12-11 01:13:56 +0100

    Improve exceptions

--- GW2SDK/Http/ResourceNotFoundException.cs
+++ GW2SDK/Http/ResourceNotFoundException.cs
@@ -4,13 +4,26 @@
 [PublicAPI]
 public sealed class ResourceNotFoundException : Exception
 {
+    /// <summary>Initializes a new instance of the <see cref="ResourceNotFoundException" /> class.</summary>
+    public ResourceNotFoundException()
+    {
+    }
+
+    /// <summary>Initializes a new instance of the <see cref="ResourceNotFoundException" /> class with a specified error
+    /// message.</summary> 
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public ResourceNotFoundException(string? message)
         : base(message)
     {
     }
 
-    public ResourceNotFoundException(string? message, Exception? inner)
-        : base(message, inner)
+    /// <summary>Initializes a new instance of the <see cref="ResourceNotFoundException" /> class with a specified error
+    /// message and a reference to the inner exception that is the cause of this exception.</summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception
+    /// is specified.</param>
+    public ResourceNotFoundException(string? message, Exception? innerException)
+        : base(message, innerException)
     {
     }
 }
--- GW2SDK/Http/UnauthorizedOperationException.cs
+++ GW2SDK/Http/UnauthorizedOperationException.cs
@@ -1,17 +1,26 @@
 ﻿namespace GuildWars2.Http;
 
+/// <summary>Thrown when the server returns an Unauthorized or Forbidden result (401 or 403).</summary>
 [PublicAPI]
 public sealed class UnauthorizedOperationException : InvalidOperationException
 {
+    /// <summary>Initializes a new instance of the <see cref="UnauthorizedOperationException" /> class.</summary>
     public UnauthorizedOperationException()
     {
     }
 
+    /// <summary>Initializes a new instance of the <see cref="UnauthorizedOperationException" /> class with a specified error
+    /// message.</summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public UnauthorizedOperationException(string? message)
         : base(message)
     {
     }
 
+    /// <summary>Initializes a new instance of the <see cref="UnauthorizedOperationException" /> class with a specified error
+    /// message and a reference to the inner exception that is the cause of this exception.</summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public UnauthorizedOperationException(string? message, Exception? innerException)
         : base(message, innerException)
     {
