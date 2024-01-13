a62e68f2a9f66e72a2f4c52a7453a48a73a78120
Steven Liekens <steven.liekens@gmail.com>
2024-01-13 14:04:15 +0000

    Add SkillBar.SelectedSkillIds

--- GW2SDK/Features/Hero/Builds/SkillBar.cs
+++ GW2SDK/Features/Hero/Builds/SkillBar.cs
@@ -19,4 +19,34 @@ public sealed record SkillBar
 
     /// <summary>The ID of the elite skill or <c>null</c> if no elite skill was selected.</summary>
     public required int? EliteSkillId { get; init; }
+
+    /// <summary>Gets the IDs of the selected skills.</summary>
+    /// <returns>The IDs of the selected skills.</returns>
+    public IEnumerable<int> SelectedSkillIds()
+    {
+        if (HealSkillId.HasValue)
+        {
+            yield return HealSkillId.Value;
+        }
+
+        if (UtilitySkillId.HasValue)
+        {
+            yield return UtilitySkillId.Value;
+        }
+
+        if (UtilitySkillId2.HasValue)
+        {
+            yield return UtilitySkillId2.Value;
+        }
+
+        if (UtilitySkillId3.HasValue)
+        {
+            yield return UtilitySkillId3.Value;
+        }
+
+        if (EliteSkillId.HasValue)
+        {
+            yield return EliteSkillId.Value;
+        }
+    }
 }
