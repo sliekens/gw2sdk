caef6b610633cfb59ac4d83339b1ef5ff9a5da96
Steven Liekens <steven.liekens@gmail.com>
2024-02-10 22:05:02 +0100

    Add jade bot skin doc comments

--- GW2SDK/Features/Hero/Equipment/JadeBots/JadeBotsClient.cs
+++ GW2SDK/Features/Hero/Equipment/JadeBots/JadeBotsClient.cs
@@ -16,6 +16,11 @@ public sealed class JadeBotsClient
 
     #region v2/account/jadebots
 
+    /// <summary>Retrieves the IDs of jade bot skins unlocked on the account associated with the access token. This endpoint is
+    /// only accessible with a valid access token.</summary>
+    /// <param name="accessToken">An API key or subtoken.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<int> Value, MessageContext Context)> GetUnlockedJadeBotSkins(
         string? accessToken,
         CancellationToken cancellationToken = default
@@ -29,6 +34,11 @@ public sealed class JadeBotsClient
 
     #region v2/jadebots
 
+    /// <summary>Retrieves all jade bot skins.</summary>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<JadeBotSkin> Value, MessageContext Context)> GetJadeBotSkins(
         Language? language = default,
         MissingMemberBehavior missingMemberBehavior = default,
@@ -43,6 +53,9 @@ public sealed class JadeBotsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves the IDs of all jade bot skins.</summary>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<int> Value, MessageContext Context)> GetJadeBotSkinsIndex(
         CancellationToken cancellationToken = default
     )
@@ -51,6 +64,12 @@ public sealed class JadeBotsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a jade bot skin by its ID.</summary>
+    /// <param name="jadeBotSkinId">The jade bot skin ID.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(JadeBotSkin Value, MessageContext Context)> GetJadeBotSkinById(
         int jadeBotSkinId,
         Language? language = default,
@@ -66,6 +85,12 @@ public sealed class JadeBotsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves jade bot skins by their IDs.</summary>
+    /// <param name="jadeBotSkinIds">The jade bot skin IDs.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<JadeBotSkin> Value, MessageContext Context)> GetJadeBotSkinsByIds(
         IReadOnlyCollection<int> jadeBotSkinIds,
         Language? language = default,
@@ -81,6 +106,13 @@ public sealed class JadeBotsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a page of jade bot skins.</summary>
+    /// <param name="pageIndex">How many pages to skip. The first page starts at 0.</param>
+    /// <param name="pageSize">How many entries to take.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<JadeBotSkin> Value, MessageContext Context)> GetJadeBotSkinsByPage(
         int pageIndex,
         int? pageSize = default,
