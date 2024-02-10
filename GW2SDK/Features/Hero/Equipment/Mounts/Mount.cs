514cb0e43432606d60a13b4cb785f5f5e0d3d299
Steven Liekens <steven.liekens@gmail.com>
2024-02-10 22:48:47 +0100

    Rename Mount.{Skins->SkinIds}

--- GW2SDK/Features/Hero/Equipment/Mounts/Mount.cs
+++ GW2SDK/Features/Hero/Equipment/Mounts/Mount.cs
@@ -10,7 +10,7 @@ public sealed record Mount
 
     public required int DefaultSkin { get; init; }
 
-    public required IReadOnlyCollection<int> Skins { get; init; }
+    public required IReadOnlyCollection<int> SkinIds { get; init; }
 
     public required IReadOnlyCollection<SkillReference> Skills { get; init; }
 }
