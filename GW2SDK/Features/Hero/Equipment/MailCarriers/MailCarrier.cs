e62c717886cabe9cd9fbda731416fb7d518977bb
Steven Liekens <steven.liekens@gmail.com>
2024-02-10 22:24:32 +0100

    Add mail carrier doc comments

--- GW2SDK/Features/Hero/Equipment/MailCarriers/MailCarrier.cs
+++ GW2SDK/Features/Hero/Equipment/MailCarriers/MailCarrier.cs
@@ -1,9 +1,11 @@
 ﻿namespace GuildWars2.Hero.Equipment.MailCarriers;
 
+/// <summary>Information about a mail carrier.</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record MailCarrier
 {
+    /// <summary>The mail carrier ID.</summary>
     public required int Id { get; init; }
 
     /// <summary>The IDs of the items that unlock the mail carrier when consumed.</summary>
@@ -15,7 +17,9 @@ public sealed record MailCarrier
     /// <summary>The URL of the mail carrier icon.</summary>
     public required string IconHref { get; init; }
 
+    /// <summary>The name of the mail carrier.</summary>
     public required string Name { get; init; }
 
+    /// <summary>Contains various modifiers.</summary>
     public required MailCarrierFlags Flags { get; init; }
 }
--- GW2SDK/Features/Hero/Equipment/MailCarriers/MailCarriersClient.cs
+++ GW2SDK/Features/Hero/Equipment/MailCarriers/MailCarriersClient.cs
@@ -2,7 +2,7 @@
 
 namespace GuildWars2.Hero.Equipment.MailCarriers;
 
-/// <summary>Provides query methods for mail carrier animations and animations unlocked on the account.</summary>
+/// <summary>Provides query methods for mail carriers and mail carriers unlocked on the account.</summary>
 [PublicAPI]
 public sealed class MailCarriersClient
 {
@@ -14,6 +14,31 @@ public sealed class MailCarriersClient
         httpClient.BaseAddress ??= BaseAddress.DefaultUri;
     }
 
+    #region v2/account/mailcarriers
+
+    /// <summary>Retrieves the IDs of mail carriers unlocked on the account associated with the access token. This endpoint is
+    /// only accessible with a valid access token.</summary>
+    /// <param name="accessToken">An API key or subtoken.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
+    public Task<(HashSet<int> Value, MessageContext Context)> GetUnlockedMailCarriers(
+        string? accessToken,
+        CancellationToken cancellationToken = default
+    )
+    {
+        UnlockedMailCarriersRequest request = new() { AccessToken = accessToken };
+        return request.SendAsync(httpClient, cancellationToken);
+    }
+
+    #endregion v2/account/mailcarriers
+
+    #region v2/mailcarriers
+
+    /// <summary>Retrieves all mail carriers.</summary>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MailCarrier> Value, MessageContext Context)> GetMailCarriers(
         Language? language = default,
         MissingMemberBehavior missingMemberBehavior = default,
@@ -28,6 +53,9 @@ public sealed class MailCarriersClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves the IDs of all mail carriers.</summary>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<int> Value, MessageContext Context)> GetMailCarriersIndex(
         CancellationToken cancellationToken = default
     )
@@ -36,6 +64,12 @@ public sealed class MailCarriersClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a mail carrier by its ID.</summary>
+    /// <param name="mailCarrierId">The mail carrier ID.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(MailCarrier Value, MessageContext Context)> GetMailCarrierById(
         int mailCarrierId,
         Language? language = default,
@@ -51,6 +85,12 @@ public sealed class MailCarriersClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves mail carriers by their IDs.</summary>
+    /// <param name="mailCarrierIds">The mail carrier IDs.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MailCarrier> Value, MessageContext Context)> GetMailCarriersByIds(
         IReadOnlyCollection<int> mailCarrierIds,
         Language? language = default,
@@ -66,6 +106,13 @@ public sealed class MailCarriersClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a page of mail carriers.</summary>
+    /// <param name="pageIndex">How many pages to skip. The first page starts at 0.</param>
+    /// <param name="pageSize">How many entries to take.</param>
+    /// <param name="language">The language to use for descriptions.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MailCarrier> Value, MessageContext Context)> GetMailCarriersByPage(
         int pageIndex,
         int? pageSize = default,
@@ -83,12 +130,5 @@ public sealed class MailCarriersClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
-    public Task<(HashSet<int> Value, MessageContext Context)> GetUnlockedMailCarriers(
-        string? accessToken,
-        CancellationToken cancellationToken = default
-    )
-    {
-        UnlockedMailCarriersRequest request = new() { AccessToken = accessToken };
-        return request.SendAsync(httpClient, cancellationToken);
-    }
+    #endregion v2/mailcarriers
 }
