9d8723380152f2c785df16ab2d1d16892f20c9dd
Steven Liekens <steven.liekens@gmail.com>
2024-02-18 00:20:25 +0100

    Fix build

--- GW2SDK/Features/Hero/Equipment/AttributesJson.cs
+++ GW2SDK/Features/Hero/Equipment/AttributesJson.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json;
-using GuildWars2.Json;
 
 namespace GuildWars2.Hero.Equipment;
 
@@ -13,28 +12,50 @@ internal static class AttributesJson
         var result = new Dictionary<AttributeName, int>(4);
         foreach (var member in json.EnumerateObject())
         {
-            var value = member.Value.GetInt32();
-
             // Somemetimes the old attribute names (or partial names) are used in the API
-            if (member.NameEquals("BoonDuration"))
+            if (member.NameEquals("Power"))
             {
-                result[AttributeName.Concentration] = value;
+                result[AttributeName.Power] = member.Value.GetInt32();
+            }
+            else if (member.NameEquals("Precision"))
+            {
+                result[AttributeName.Precision] = member.Value.GetInt32();
+            }
+            else if (member.NameEquals("Toughness"))
+            {
+                result[AttributeName.Toughness] = member.Value.GetInt32();
+            }
+            else if (member.NameEquals("Vitality"))
+            {
+                result[AttributeName.Vitality] = member.Value.GetInt32();
+            }
+            else if (member.NameEquals("BoonDuration"))
+            {
+                result[AttributeName.Concentration] = member.Value.GetInt32();
+            }
+            else if (member.NameEquals("ConditionDamage"))
+            {
+                result[AttributeName.ConditionDamage] = member.Value.GetInt32();
             }
             else if (member.NameEquals("CritDamage"))
             {
-                result[AttributeName.Ferocity] = value;
+                result[AttributeName.Ferocity] = member.Value.GetInt32();
             }
             else if (member.NameEquals("ConditionDuration"))
             {
-                result[AttributeName.Expertise] = value;
+                result[AttributeName.Expertise] = member.Value.GetInt32();
             }
             else if (member.NameEquals("Healing"))
             {
-                result[AttributeName.HealingPower] = value;
+                result[AttributeName.HealingPower] = member.Value.GetInt32();
             }
-            else
+            else if (member.NameEquals("AgonyResistance"))
             {
-                result[json.GetEnum<AttributeName>(missingMemberBehavior)] = value;
+                result[AttributeName.AgonyResistance] = member.Value.GetInt32();
+            }
+            else if (missingMemberBehavior == MissingMemberBehavior.Error)
+            {
+                throw new InvalidOperationException(Strings.UnexpectedMember(member.Name));
             }
         }
 
