a16599f4773c419978d7a8fce3683b5c08267add
Steven Liekens <steven.liekens@gmail.com>
2024-02-17 21:53:03 +0100

    Reorder attribute names

--- GW2SDK/Features/Hero/AttributeName.cs
+++ GW2SDK/Features/Hero/AttributeName.cs
@@ -11,20 +11,8 @@ public enum AttributeName
     /// <summary>No attribute.</summary>
     None,
 
-    /// <summary>Concentration increases outgoing boon duration, including boons applied to self. Secondary attribute.</summary>
-    Concentration,
-
-    /// <summary>Ferocity increases outgoing critical damage. Secondary attribute.</summary>
-    Ferocity,
-
-    /// <summary>Condition damage increases outgoing condition damage. Secondary attribute.</summary>
-    ConditionDamage,
-
-    /// <summary>Expertise increases outgoing condition duration. Secondary attribute.</summary>
-    Expertise,
-
-    /// <summary>Healing power increases outgoing healing, including self-heals. Secondary attribute.</summary>
-    HealingPower,
+    /// <summary>Power increases outgoing strike damage. Primary attribute.</summary>
+    Power,
 
     /// <summary>Precision increases critical chance. Primary attribute.</summary>
     Precision,
@@ -32,11 +20,23 @@ public enum AttributeName
     /// <summary>Toughness increases armor. Primary attribute.</summary>
     Toughness,
 
-    /// <summary>Power increases outgoing strike damage. Primary attribute.</summary>
-    Power,
-
     /// <summary>Vitality increases health. Primary attribute.</summary>
     Vitality,
+
+    /// <summary>Concentration increases outgoing boon duration, including boons applied to self. Secondary attribute.</summary>
+    Concentration,
+
+    /// <summary>Condition damage increases outgoing condition damage. Secondary attribute.</summary>
+    ConditionDamage,
+
+    /// <summary>Expertise increases outgoing condition duration. Secondary attribute.</summary>
+    Expertise,
+
+    /// <summary>Ferocity increases outgoing critical damage. Secondary attribute.</summary>
+    Ferocity,
+
+    /// <summary>Healing power increases outgoing healing, including self-heals. Secondary attribute.</summary>
+    HealingPower,
 
     /// <summary>Agony Resistance improves resistance against Agony; only relevant inside Fractals of the Mists. Special
     /// attribute.</summary>
