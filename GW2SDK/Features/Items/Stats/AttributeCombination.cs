492d159724177d73a2fdbab59a3bb02df5d31696
Steven Liekens <steven.liekens@gmail.com>
2024-02-17 22:16:23 +0100

    Add Attribute doc comments

--- GW2SDK/Features/Items/Stats/AttributeCombination.cs
+++ GW2SDK/Features/Items/Stats/AttributeCombination.cs
@@ -1,12 +1,16 @@
 ﻿namespace GuildWars2.Items.Stats;
 
+/// <summary>Information about an item attribute combination like Soldier.</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record AttributeCombination
 {
+    /// <summary>The ID of the attribute combination.</summary>
     public required int Id { get; init; }
 
+    /// <summary>The name of the attribute combination.</summary>
     public required string Name { get; init; }
 
+    /// <summary>The attributes values which are used to calculate the final stats of an item.</summary>
     public required IReadOnlyCollection<Attribute> Attributes { get; init; }
 }
