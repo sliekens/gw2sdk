a1989cc455a3fac507495549bed2b3b647c0f7f9
Steven Liekens <steven.liekens@gmail.com>
2024-02-22 22:05:44 +0100

    Add GameLink doc comments

--- GW2SDK/Features/GameLink.cs
+++ GW2SDK/Features/GameLink.cs
@@ -4,6 +4,7 @@ using GuildWars2.Mumble;
 
 namespace GuildWars2;
 
+/// <summary>Represents a link to the game client that provides realtime game state updates.</summary>
 [PublicAPI]
 public sealed class GameLink : IObservable<GameTick>, IDisposable
 {
@@ -27,6 +28,9 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
     /// <summary>Used to keep track of the last UiTick that was published, to prevent repeating the same state.</summary>
     private uint lastTick;
 
+    /// <summary>Initializes a new instance of the <see cref="GameLink" /> class.</summary>
+    /// <param name="mumbleLink">The memory-mapped file used by the game client.</param>
+    /// <param name="refreshInterval">The interval at which to poll for changes to the shared memory.</param>
     private GameLink(MumbleLink mumbleLink, TimeSpan refreshInterval)
     {
         this.mumbleLink = mumbleLink;
@@ -38,6 +42,7 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         );
     }
 
+    /// <summary>Disposes the <see cref="GameLink" /> instance and releases any resources used.</summary>
     public void Dispose()
     {
         if (disposed)
@@ -70,6 +75,9 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         }
     }
 
+    /// <summary>Subscribes an observer to receive realtime game state updates.</summary>
+    /// <param name="observer">The observer to subscribe.</param>
+    /// <returns>An IDisposable object that can be used to unsubscribe the observer.</returns>
     public IDisposable Subscribe(IObserver<GameTick> observer)
     {
         if (disposed)
@@ -100,6 +108,8 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         return new Subscription(this, observer);
     }
 
+    /// <summary>Gets a snapshot of the current game state.</summary>
+    /// <returns>A <see cref="GameTick" /> object representing the current game state.</returns>
     public GameTick GetSnapshot()
     {
         if (disposed)
@@ -110,6 +120,7 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         return mumbleLink.GetValue<GameTick>();
     }
 
+    /// <summary>Publishes the current game state to all subscribed observers.</summary>
     private void Publish()
     {
         if (busy || subscribers.Count == 0)
@@ -182,9 +193,15 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         }
     }
 
+    /// <summary>Checks if the GameLink is supported on the current platform.</summary>
+    /// <returns><c>true</c> if the GameLink is supported on the current platform; otherwise, <c>false</c>.</returns>
     [SupportedOSPlatformGuard("windows")]
     public static bool IsSupported() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
+    /// <summary>Opens a GameLink instance with the specified refresh interval and name.</summary>
+    /// <param name="refreshInterval">The interval at which to poll for changes to the shared memory.</param>
+    /// <param name="name">The name of the memory-mapped file.</param>
+    /// <returns>A new instance of the <see cref="GameLink" /> class.</returns>
     [SupportedOSPlatform("windows")]
     public static GameLink Open(TimeSpan refreshInterval = default, string name = "MumbleLink")
     {
@@ -197,8 +214,10 @@ public sealed class GameLink : IObservable<GameTick>, IDisposable
         return new GameLink(link, refreshInterval);
     }
 
+    /// <summary>Represents a subscription to receive realtime game state updates.</summary>
     private class Subscription(GameLink producer, IObserver<GameTick> observer) : IDisposable
     {
+        /// <summary>Disposes the subscription and removes the observer from the subscribers list.</summary>
         public void Dispose() => producer.subscribers.Remove(observer);
     }
 }
