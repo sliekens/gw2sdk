c55bb635ad7a39bc4ca47be5b959be22c83cbc9f
Steven Liekens <steven.liekens@gmail.com>
2024-03-03 14:47:24 +0100

    Change Rune.Bonuses to IReadOnlyList

--- GW2SDK/Features/Items/Models/UpgradeComponents/Rune.cs
+++ GW2SDK/Features/Items/Models/UpgradeComponents/Rune.cs
@@ -3,5 +3,9 @@
 [PublicAPI]
 public sealed record Rune : UpgradeComponent
 {
-    public required IReadOnlyCollection<string>? Bonuses { get; init; }
+    /// <summary>The bonuses provided by this rune.</summary>
+    /// <remarks>Each equipped rune gives one bonus from this list. The bonuses are cumulative and depend on the number of
+    /// identical runes equipped. For example, if you have two runes of the same type equipped, you will get the first two
+    /// bonuses from the list. If you have three of the same runes equipped, you will get the first three bonuses, and so on.</remarks>
+    public required IReadOnlyList<string>? Bonuses { get; init; }
 }
