3837227fff10bbd8c75f5e00a1349c2e556fa9fa
Steven Liekens <steven.liekens@gmail.com>
2024-03-10 23:28:40 +0100

    Change BaseAddress.Default from static readonly string to const string

--- GW2SDK/Features/BaseAddress.cs
+++ GW2SDK/Features/BaseAddress.cs
@@ -4,9 +4,9 @@
 [PublicAPI]
 public static class BaseAddress
 {
-    /// <summary>"https://api.guildwars2.com"</summary>
-    public static readonly string Default = "https://api.guildwars2.com";
+    /// <summary>The default base address to use for API requests.</summary>
+    public const string Default = "https://api.guildwars2.com";
 
-    /// <summary>"https://api.guildwars2.com"</summary>
+    /// <summary>The default base address to use for API requests.</summary>
     public static readonly Uri DefaultUri = new(Default, UriKind.Absolute);
 }
