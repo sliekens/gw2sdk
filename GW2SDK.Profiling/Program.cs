d8ef74021368e1d01a1744f674c917299c5610e7
Steven Liekens <steven.liekens@gmail.com>
2024-03-12 01:02:44 +0100

    Add Profiling thing

---
+++ GW2SDK.Profiling/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is just some code to test the performance of the GW2SDK library
+// VS2022 has a built-in profiler (alt + F2), so I'm using that to see how the library performs 
+
+using GuildWars2;
+
+using var httpClient = new HttpClient();
+var gw2Client = new Gw2Client(httpClient);
+
+await foreach (var (recipe, _) in gw2Client.Hero.Crafting.Recipes.GetRecipesBulk())
+{
+    Console.WriteLine(recipe.Id);
+}
