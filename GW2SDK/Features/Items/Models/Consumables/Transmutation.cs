6eb1c76e9bfc1a3d4d0e1151e422d7b773e7744c
Steven Liekens <steven.liekens@gmail.com>
2024-03-17 01:21:10 +0100

    Add Transmutation.GetSkinChatLinks method

--- GW2SDK/Features/Items/Models/Consumables/Transmutation.cs
+++ GW2SDK/Features/Items/Models/Consumables/Transmutation.cs
@@ -1,7 +1,14 @@
-﻿namespace GuildWars2.Items;
+﻿using GuildWars2.Chat;
+
+namespace GuildWars2.Items;
 
 [PublicAPI]
 public sealed record Transmutation : Consumable
 {
     public required IReadOnlyCollection<int> SkinIds { get; init; }
+
+    /// <summary>Gets chat link objects for the skins.</summary>
+    /// <returns>The chat links as objects.</returns>
+    public IEnumerable<SkinLink> GetSkinChatLinks() =>
+        SkinIds.Select(id => new SkinLink { SkinId = id });
 }
