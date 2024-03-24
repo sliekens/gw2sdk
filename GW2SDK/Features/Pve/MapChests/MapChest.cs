f8572861b6cf233ae54f3516c5fceb717bb15537
Steven Liekens <steven.liekens@gmail.com>
2024-03-24 17:15:18 +0100

    Add map chest doc comments

--- GW2SDK/Features/Pve/MapChests/MapChest.cs
+++ GW2SDK/Features/Pve/MapChests/MapChest.cs
@@ -1,8 +1,10 @@
 ﻿namespace GuildWars2.Pve.MapChests;
 
+/// <summary>Information about a map chest which can be earned once per day through meta-event completion.</summary>
 [PublicAPI]
 [DataTransferObject]
 public sealed record MapChest
 {
+    /// <summary>The map chest ID.</summary>
     public required string Id { get; init; }
 }
--- GW2SDK/Features/Pve/MapChests/MapChestsClient.cs
+++ GW2SDK/Features/Pve/MapChests/MapChestsClient.cs
@@ -18,6 +18,11 @@ public sealed class MapChestsClient
 
     #region v2/account/mapchests
 
+    /// <summary>Retrieves the IDs of all map chests that have been received since the last server reset on the account
+    /// associated with the access token. This endpoint is only accessible with a valid access token.</summary>
+    /// <param name="accessToken">An API key or subtoken.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<string> Value, MessageContext Context)> GetReceivedMapChests(
         string? accessToken,
         CancellationToken cancellationToken = default
@@ -31,6 +36,9 @@ public sealed class MapChestsClient
 
     #region v2/mapchests
 
+    /// <summary>Retrieves the IDs of all map chests.</summary>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<string> Value, MessageContext Context)> GetMapChestsIndex(
         CancellationToken cancellationToken = default
     )
@@ -39,6 +47,11 @@ public sealed class MapChestsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a map chest by its ID.</summary>
+    /// <param name="mapChestId">The map chest ID.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(MapChest Value, MessageContext Context)> GetMapChestById(
         string mapChestId,
         MissingMemberBehavior missingMemberBehavior = default,
@@ -52,6 +65,11 @@ public sealed class MapChestsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves map chests by their IDs.</summary>
+    /// <param name="mapChestIds">The map chest IDs.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MapChest> Value, MessageContext Context)> GetMapChestsByIds(
         IReadOnlyCollection<string> mapChestIds,
         MissingMemberBehavior missingMemberBehavior = default,
@@ -65,6 +83,12 @@ public sealed class MapChestsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves a page of map chests.</summary>
+    /// <param name="pageIndex">How many pages to skip. The first page starts at 0.</param>
+    /// <param name="pageSize">How many entries to take.</param>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MapChest> Value, MessageContext Context)> GetMapChestsByPage(
         int pageIndex,
         int? pageSize = default,
@@ -81,6 +105,10 @@ public sealed class MapChestsClient
         return request.SendAsync(httpClient, cancellationToken);
     }
 
+    /// <summary>Retrieves all map chests.</summary>
+    /// <param name="missingMemberBehavior">The desired behavior when JSON contains unexpected members.</param>
+    /// <param name="cancellationToken">A token to cancel the request.</param>
+    /// <returns>A task that represents the API request.</returns>
     public Task<(HashSet<MapChest> Value, MessageContext Context)> GetMapChests(
         MissingMemberBehavior missingMemberBehavior = default,
         CancellationToken cancellationToken = default
