d1f5ff7b979ff71f0c131830b8e498190035d332
Steven Liekens <steven.liekens@gmail.com>
2024-03-29 21:40:39 +0100

    Add Extensible enum

---
+++ GW2SDK.Tests/Features/ExtensibleEnum.cs
@@ -0,0 +1,84 @@
+﻿using GuildWars2.Items;
+
+namespace GuildWars2.Tests.Features;
+
+public class ExtensibleEnum
+{
+    [Fact]
+    public void Indicates_when_enum_name_is_defined()
+    {
+        var extensible = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        Assert.True(extensible.IsDefined());
+    }
+
+    [Fact]
+    public void Indicates_when_enum_name_is_not_defined()
+    {
+        var extensible = new Extensible<Rarity>("Mythical");
+        Assert.False(extensible.IsDefined());
+    }
+
+    [Fact]
+    public void Returns_enum_name_as_string()
+    {
+        var extensible = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        Assert.Equal(nameof(Rarity.Legendary), extensible.ToString());
+    }
+
+    [Fact]
+    public void Implicitly_converts_enum_to_extensible_enum()
+    {
+        Extensible<Rarity> extensible = Rarity.Legendary;
+        Assert.Equal(nameof(Rarity.Legendary), extensible.ToString());
+    }
+
+    [Fact]
+    public void Implicitly_converts_string_to_extensible_enum()
+    {
+        Extensible<Rarity> extensible = nameof(Rarity.Legendary);
+        Assert.Equal(nameof(Rarity.Legendary), extensible.ToString());
+    }
+
+    [Fact]
+    public void Converts_null_to_default_enum_value()
+    {
+        Extensible<MissingMemberBehavior> extensible = default;
+        Assert.Equal("Error", extensible.ToString());
+    }
+
+    [Fact]
+    public void Indicates_when_value_equals_other_value()
+    {
+        var left = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        var right = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        Assert.True(left == right);
+        Assert.True(right == left);
+    }
+
+    [Fact]
+    public void Indicates_when_value_equals_enum_value()
+    {
+        var left = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        var right = Rarity.Legendary;
+        Assert.True(left == right);
+        Assert.True(right == left);
+    }
+
+    [Fact]
+    public void Indicates_when_value_equals_string_value()
+    {
+        var left = new Extensible<Rarity>(nameof(Rarity.Legendary));
+        var right = nameof(Rarity.Legendary);
+        Assert.True(left == right);
+        Assert.True(right == left);
+    }
+
+    [Fact]
+    public void Does_case_insensitivate_equality_check()
+    {
+        var left = new Extensible<Rarity>(nameof(Rarity.Legendary).ToLowerInvariant());
+        var right = new Extensible<Rarity>(nameof(Rarity.Legendary).ToUpperInvariant());
+        Assert.True(left == right);
+        Assert.True(right == left);
+    }
+}
---
+++ GW2SDK/Features/Extensible.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace GuildWars2;
+
+/// <summary>Represents an enum that can be extended with additional values.</summary>
+/// <typeparam name="TEnum">The type of the enum.</typeparam>
+[PublicAPI]
+[DebuggerDisplay("{ToString(),nq}")]
+public struct Extensible<TEnum>(string Name) where TEnum : Enum
+{
+    /// <summary>Determines whether the current name is defined in the enum.</summary>
+    /// <returns><c>true</c> if the name is defined in the enum; otherwise, <c>false</c>.</returns>
+    public bool IsDefined() => Enum.IsDefined(typeof(TEnum), ToString());
+
+    /// <summary>Determines whether the current instance of Extensible is equal to another Extensible object.</summary>
+    /// <param name="other">The Extensible object to compare with the current instance.</param>
+    /// <returns><c>true</c> if the current instance is equal to the other object; otherwise, <c>false</c>.</returns>
+    public bool Equals(Extensible<TEnum> other) =>
+        string.Equals(ToString(), other.ToString(), StringComparison.OrdinalIgnoreCase);
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => obj is Extensible<TEnum> other && Equals(other);
+
+    /// <inheritdoc />
+    public override int GetHashCode() => StringComparer.OrdinalIgnoreCase.GetHashCode(ToString());
+
+    /// <inheritdoc />
+    public override string ToString() => Name ?? default(TEnum)!.ToString();
+
+    /// <summary>Determines whether two Extensible objects are equal.</summary>
+    /// <param name="left">The first Extensible object to compare.</param>
+    /// <param name="right">The second Extensible object to compare.</param>
+    /// <returns><c>true</c> if the two objects are equal; otherwise, <c>false</c>.</returns>
+    public static bool operator ==(Extensible<TEnum> left, Extensible<TEnum> right) =>
+        left.Equals(right);
+
+    /// <summary>Determines whether two Extensible objects are not equal.</summary>
+    /// <param name="left">The first Extensible object to compare.</param>
+    /// <param name="right">The second Extensible object to compare.</param>
+    /// <returns><c>true</c> if the two objects are not equal; otherwise, <c>false</c>.</returns>
+    public static bool operator !=(Extensible<TEnum> left, Extensible<TEnum> right) =>
+        !left.Equals(right);
+
+    /// <summary>Implicitly converts a string to an instance of the Extensible enum class.</summary>
+    /// <param name="name">The name of the enum value.</param>
+    /// <returns>An instance of the Extensible enum class.</returns>
+    public static implicit operator Extensible<TEnum>(string name) => new(name);
+
+    /// <summary>Implicitly converts an enum to an instance of the Extensible enum class.</summary>
+    /// <param name="name">The enum value.</param>
+    /// <returns>An instance of the Extensible enum class.</returns>
+    public static implicit operator Extensible<TEnum>(TEnum name) => new(name.ToString());
+}
