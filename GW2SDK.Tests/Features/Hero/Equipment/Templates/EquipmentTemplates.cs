a3c8c5282faf5a40bf37b92a97f7c83bab4ccf84
Steven Liekens <steven.liekens@gmail.com>
2024-05-11 11:50:41 +0200

    Improve EquipmentTemplates test

--- GW2SDK.Tests/Features/Hero/Equipment/Templates/EquipmentTemplates.cs
+++ GW2SDK.Tests/Features/Hero/Equipment/Templates/EquipmentTemplates.cs
@@ -11,11 +11,17 @@ public class EquipmentTemplates
         var character = Composer.Resolve<TestCharacter>();
         var accessToken = Composer.Resolve<ApiKey>();
 
-        var (actual, _) =
+        var (actual, context) =
             await sut.Hero.Equipment.Templates.GetEquipmentTemplates(
                 character.Name,
                 accessToken.Key
             );
+
+        Assert.NotNull(context.Links);
+        Assert.Equal(50, context.PageSize);
+        Assert.InRange(context.ResultCount.GetValueOrDefault(), 1, 8);
+        Assert.True(context.PageTotal > 0);
+        Assert.True(context.ResultTotal > 0);
 
         Assert.NotEmpty(actual);
         Assert.All(
