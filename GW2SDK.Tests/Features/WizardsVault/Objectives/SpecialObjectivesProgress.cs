91b984fa7709b50689181f48d5116c1a14950fa9
Steven Liekens <steven.liekens@gmail.com>
2024-06-08 01:33:31 +0200

    Re-enable wizards vault tests

--- GW2SDK.Tests/Features/WizardsVault/Objectives/SpecialObjectivesProgress.cs
+++ GW2SDK.Tests/Features/WizardsVault/Objectives/SpecialObjectivesProgress.cs
@@ -4,7 +4,7 @@ namespace GuildWars2.Tests.Features.WizardsVault.Objectives;
 
 public class SpecialObjectivesProgress
 {
-    [Fact(Skip = "API returns no results for the new season")]
+    [Fact]
     public async Task Can_be_listed()
     {
         var sut = Composer.Resolve<Gw2Client>();
@@ -25,20 +25,5 @@ public class SpecialObjectivesProgress
                 Assert.True(objective.RewardAcclaim > 0);
             }
         );
-    }
-
-    // Test to prove that the API is misbehaving and the test above should be skipped
-    // When this test is removed, the test above should be re-enabled by removing the Skip attribute
-    [Fact]
-    public async Task Cannot_be_listed()
-    {
-        var sut = Composer.Resolve<Gw2Client>();
-        var accessToken = TestConfiguration.ApiKey;
-
-        var (actual, context) =
-            await sut.WizardsVault.GetSpecialObjectivesProgress(accessToken.Key);
-
-        Assert.NotNull(context);
-        Assert.Empty(actual.Objectives);
     }
 }
--- GW2SDK.Tests/Features/WizardsVault/Seasons/Objectives.cs
+++ GW2SDK.Tests/Features/WizardsVault/Seasons/Objectives.cs
@@ -1,11 +1,10 @@
-﻿using GuildWars2.Http;
-using GuildWars2.Tests.TestInfrastructure;
+﻿using GuildWars2.Tests.TestInfrastructure;
 
 namespace GuildWars2.Tests.Features.WizardsVault.Seasons;
 
 public class Season
 {
-    [Fact(Skip = "API returns an unknown error")]
+    [Fact]
     public async Task Can_be_found()
     {
         var sut = Composer.Resolve<Gw2Client>();
@@ -18,17 +17,5 @@ public class Season
         Assert.True(actual.End > actual.Start);
         Assert.NotEmpty(actual.AstralRewardIds);
         Assert.NotEmpty(actual.ObjectiveIds);
-    }
-
-    // Test to prove that the API is misbehaving and the test above should be skipped
-    // When this test is removed, the test above should be re-enabled by removing the Skip attribute
-    [Fact]
-    public async Task Returns_unknown_error()
-    {
-        var sut = Composer.Resolve<Gw2Client>();
-
-        var error = await Assert.ThrowsAsync<BadResponseException>(async () => await sut.WizardsVault.GetSeason());
-
-        Assert.Equal("unknown error", error.Message);
     }
 }
