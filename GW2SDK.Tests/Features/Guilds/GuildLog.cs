7b2abecde636283c66698075b88f0935eb42e294
Steven Liekens <steven.liekens@gmail.com>
2024-10-03 21:46:11 +0000

    Re-enable guild log test

--- GW2SDK.Tests/Features/Guilds/GuildLog.cs
+++ GW2SDK.Tests/Features/Guilds/GuildLog.cs
@@ -6,7 +6,7 @@ namespace GuildWars2.Tests.Features.Guilds;
 
 public class GuildLog
 {
-    [Fact(Skip = "API returns a gateway error")]
+    [Fact]
     public async Task Can_be_found()
     {
         var sut = Composer.Resolve<Gw2Client>();
@@ -52,28 +52,5 @@ public class GuildLog
                 Assert.All(range, log => Assert.True(log.Id > skipToken));
             }
         }
-    }
-
-    // Test to prove that the API is misbehaving and the test above should be skipped
-    // When this test is removed, the test above should be re-enabled by removing the Skip attribute
-    [Fact]
-    public async Task Returns_Gateway_Error()
-    {
-        var sut = Composer.Resolve<Gw2Client>();
-        var guildLeader = TestConfiguration.TestGuildLeader;
-
-        var account = await sut.Hero.Account.GetSummary(guildLeader.Token).ValueOnly();
-
-        Exception? error = null;
-        foreach (var guildId in account.LeaderOfGuildIds!)
-        {
-            error = await Record.ExceptionAsync(
-                async () => await sut.Guilds.GetGuildLog(guildId, guildLeader.Token)
-            );
-
-            if (error is not null) break;
-        }
-
-        Assert.IsType<TimeoutRejectedException>(error);
     }
 }
