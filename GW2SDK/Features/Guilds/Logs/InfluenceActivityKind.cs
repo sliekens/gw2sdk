19e7aafa7a4ea7f062c45219ebb712cc36cd23c6
Steven Liekens <steven.liekens@gmail.com>
2024-10-18 15:17:03 +0000

    Add InfluenceActivityKind.DailyLogin

--- GW2SDK/Features/Guilds/Logs/InfluenceActivityKind.cs
+++ GW2SDK/Features/Guilds/Logs/InfluenceActivityKind.cs
@@ -5,5 +5,8 @@
 public enum InfluenceActivityKind
 {
     /// <summary>A guild member gifted influence to the guild.</summary>
-    Gifted = 1
+    Gifted = 1,
+
+    /// <summary>Influence was awarded to the guild by each character as it logs in. The guild's attendance log was only updated once per day.</summary>
+    DailyLogin,
 }
