5b7d7ec377857f13d83595776eeaee96fad69247
Steven Liekens <steven.liekens@gmail.com>
2024-10-22 20:28:44 +0000

    Change MarkupTextConverterTest to not depend on API

--- GW2SDK.Tests/Features/Markup/MarkupTextConverterTest.cs
+++ GW2SDK.Tests/Features/Markup/MarkupTextConverterTest.cs
@@ -1,21 +1,29 @@
 ﻿using GuildWars2.Markup;
-using GuildWars2.Tests.TestInfrastructure;
 
 namespace GuildWars2.Tests.Features.Markup;
 
 public class MarkupTextConverterTest
 {
     [Fact]
-    public async Task Describe_End_of_Dragons_Launch_Supply_Drop_Requisition()
+    public void Describe_End_of_Dragons_Launch_Supply_Drop_Requisition()
     {
-        var gw2 = Composer.Resolve<Gw2Client>();
-        var item = await gw2.Items.GetItemById(97418).ValueOnly();
-
+        var input = "Use this to sign your account up for weekly End of Dragons Launch Supply " +
+                    "Drops. Receive the first drop immediately and three more between now and " +
+                    "March 21.<br><br>First Drop:<br>A Red Crane Weapon Choice, 5 Black Lion " +
+                    "Chest Keys, a Golden Black Lion Chest Key, and 5 <REDACTED> Dye Kits.<br>" +
+                    "Second Drop:<br>A Black Lion Backpack and Glider Combo Voucher, an End of " +
+                    "Dragons Expedition Board, and 2 Heroic Boosters.<br>Third Drop:<br>A Black " +
+                    "Lion Outfit Voucher, a Black Lion Weapon Voucher, 2 Fine Black Lion Dye " +
+                    "Canisters—Blue, 2 Fine Black Lion Dye Canisters—Green, 2 Fine Black Lion " +
+                    "Dye Canisters—Red, and 2 Fine Black Lion Dye Canisters—Yellow.<br>Fourth " +
+                    "Drop:<br>A Gold Essence Weapon Choice, 2 Guaranteed Wardrobe Unlocks, and " +
+                    "2 Black Lion Miniature Claim Tickets.";
         var lexer = new MarkupLexer();
         var parser = new MarkupParser();
         var converter = new MarkupTextConverter();
-
-        var actual = converter.Convert(parser.Parse(lexer.Tokenize(item.Description)));
+        var tokens = lexer.Tokenize(input);
+        var syntax = parser.Parse(tokens);
+        var actual = converter.Convert(syntax);
 
         Assert.Equal(
             """
