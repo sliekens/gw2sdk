647569e7203ca93de98300b23673d3bb5478b264
Steven Liekens <steven.liekens@gmail.com>
2024-11-20 00:43:51 +0000

    Ignore SAB decorations

--- GW2SDK.Tests/Features/Pve/Home/Decorations/Decorations.cs
+++ GW2SDK.Tests/Features/Pve/Home/Decorations/Decorations.cs
@@ -1,6 +1,4 @@
-﻿using GuildWars2.Chat;
-using GuildWars2.Hero.Equipment.Wardrobe;
-using GuildWars2.Tests.TestInfrastructure;
+﻿using GuildWars2.Tests.TestInfrastructure;
 
 namespace GuildWars2.Tests.Features.Pve.Home.Decorations;
 
@@ -20,7 +18,15 @@ public class Decorations
             Assert.True(actual.Id > 0);
             Assert.NotEmpty(actual.Name);
             Assert.NotNull(actual.Description);
-            Assert.NotEmpty(actual.CategoryIds);
+            if (actual.Id is 13 or 28 or 73 or 161 or 170 or 379 or 396 or 435 or 438 or 458 or 496 or 499 or 528 or 574 or 593 or 599 or 617 or 624 or 631 or 677 or 714 or 741) // Some SAB decorations are not linked to its category
+            {
+                Assert.Empty(actual.CategoryIds);
+            }
+            else
+            {
+                Assert.NotEmpty(actual.CategoryIds);
+            }
+
             Assert.All(actual.CategoryIds, categoryId => Assert.True(categoryId > 0));
             Assert.True(actual.MaxCount > 0);
             Assert.NotEmpty(actual.IconHref);
