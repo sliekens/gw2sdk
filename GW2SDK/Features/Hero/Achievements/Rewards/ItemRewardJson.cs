10f9de3ce767644d9217e613f99e96bcc4dda346
Steven Liekens <steven.liekens@gmail.com>
2025-02-22 02:01:18 +0100

    Fix test

--- GW2SDK/Features/Hero/Achievements/Rewards/ItemRewardJson.cs
+++ GW2SDK/Features/Hero/Achievements/Rewards/ItemRewardJson.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.Json;
 using GuildWars2.Json;
 
-namespace GuildWars2.Hero.Achievements.Rewards;
+namespace GuildWars2.Hero.Achievements;
 
 internal static class ItemRewardJson
 {
