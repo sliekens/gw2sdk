15c130bdd038633c91b1043d8c5429c8779f87d5
Steven Liekens <steven.liekens@gmail.com>
2025-04-10 22:21:52 +0000

    Update to MTP

--- test.ps1
+++ test.ps1
@@ -3,11 +3,15 @@ Remove-Item -ErrorAction Ignore -Recurse GW2SDK.Tests/TestResults/*
 
 # Run tests with coverage, additional arguments are passed on to dotnet test
 # e.g. ./test.sh --filter Colors
-# e.g. ./test.sh --framework net9.0
-dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings $args
+dotnet test --project GW2SDK.Tests --framework net9.0 -- `
+    --coverage `
+    --coverage-settings GW2SDK.Tests/coverage.settings `
+    --coverage-output-format xml `
+    --coverage-output coverage.xml `
+    "$@"
 
 # Remove reports from previous runs
 Remove-Item -ErrorAction Ignore -Recurse reports
 
 # Generate coverage report
-dotnet reportgenerator -reports:*/TestResults/*/coverage.cobertura.xml -targetdir:reports
+dotnet reportgenerator -reports:GW2SDK.Tests/bin/Debug/net9.0/TestResults/coverage.xml -targetdir:reports
--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 # Remove results from previous runs
 rm -r GW2SDK.Tests/TestResults/* || true
 
 # Run tests with coverage, additional arguments are passed on to dotnet test
 # e.g. ./test.sh --filter Colors
-# e.g. ./test.sh --framework net9.0
-dotnet test --collect:"XPlat Code Coverage" --settings coverlet.runsettings "$@"
+dotnet run --project GW2SDK.Tests --framework net9.0 -- \
+    --coverage \
+    --coverage-settings GW2SDK.Tests/coverage.settings \
+    --coverage-output-format xml \
+    --coverage-output coverage.xml \
+    "$@"
 
 # Remove reports from previous runs
 rm -r reports || true
 
 # Generate coverage report
-dotnet reportgenerator -reports:*/TestResults/*/coverage.cobertura.xml -targetdir:reports
+dotnet reportgenerator -reports:GW2SDK.Tests/bin/Debug/net9.0/TestResults/coverage.xml -targetdir:reports
