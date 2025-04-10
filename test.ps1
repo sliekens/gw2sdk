60deda257b448456f54be5ed0d22f6d88529b243
Steven Liekens <steven.liekens@gmail.com>
2025-04-10 22:48:56 +0000

    Fix test scripts

--- test.ps1
+++ test.ps1
@@ -3,12 +3,12 @@ Remove-Item -ErrorAction Ignore -Recurse GW2SDK.Tests/TestResults/*
 
 # Run tests with coverage, additional arguments are passed on to dotnet test
 # e.g. ./test.sh --filter Colors
-dotnet test --project GW2SDK.Tests --framework net9.0 -- `
+dotnet run --project GW2SDK.Tests --framework net9.0 -- `
     --coverage `
     --coverage-settings GW2SDK.Tests/coverage.settings `
     --coverage-output-format xml `
     --coverage-output coverage.xml `
-    "$@"
+    @args
 
 # Remove reports from previous runs
 Remove-Item -ErrorAction Ignore -Recurse reports
