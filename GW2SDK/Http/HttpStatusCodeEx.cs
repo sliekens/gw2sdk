c5e0345679a2e62eef85b9ecd4e40000392bc98a
Steven Liekens <steven.liekens@gmail.com>
2025-08-02 10:45:02 +0000

    Use enum field explicitly

--- GW2SDK/Http/HttpStatusCodeEx.cs
+++ GW2SDK/Http/HttpStatusCodeEx.cs
@@ -11,5 +11,9 @@ public static class HttpStatusCodeEx
 {
     /// <summary>Equivalent to HTTP status 429. TooManyRequests indicates that the user has sent too many requests in a given
     /// amount of time.</summary>
+#if NET
+    public const HttpStatusCode TooManyRequests = HttpStatusCode.TooManyRequests;
+#else
     public const HttpStatusCode TooManyRequests = (HttpStatusCode)429;
+#endif
 }
