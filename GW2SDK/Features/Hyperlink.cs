76bb9734aa58609c5a7e65438405c3bd32f70467
Steven Liekens <steven.liekens@gmail.com>
2025-08-02 21:43:15 +0000

    Mute CA1056

--- GW2SDK/Features/Hyperlink.cs
+++ GW2SDK/Features/Hyperlink.cs
@@ -25,9 +25,11 @@ public readonly record struct Hyperlink
         Uri = uri;
     }
 
+#pragma warning disable CA1056 // URI-like properties should not be strings
     /// <summary>The URL of the hyperlink.</summary>
     [Obsolete("Use the Uri property instead.")]
     public string Url { get; init; }
+#pragma warning restore CA1056 // URI-like properties should not be strings
 
     /// <summary>The URL of the hyperlink.</summary>
     public Uri? Uri { get; init; }
