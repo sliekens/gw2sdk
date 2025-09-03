4ae9db9b222508aad8629892c4adc764b7fb2fa4
Steven Liekens <steven.liekens@gmail.com>
2025-09-03 21:51:08 +0000

    Amend

--- tests/GW2SDK.Tests/Features/Hero/Accounts/Luck.cs
+++ tests/GW2SDK.Tests/Features/Hero/Accounts/Luck.cs
@@ -8,20 +8,16 @@ public class Luck
     [Fact]
     public async Task Can_be_found()
     {
-        // https://github.com/gw2-api/issues/issues/133
-        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
-        {
-            Gw2Client sut = Composer.Resolve<Gw2Client>();
-            ApiKey accessToken = TestConfiguration.ApiKey;
+        Gw2Client sut = Composer.Resolve<Gw2Client>();
+        ApiKey accessToken = TestConfiguration.ApiKey;
 
-            (AccountLuck actual, _) = await sut.Hero.Account.GetLuck(
-                accessToken.Key,
-                cancellationToken: TestContext.Current.CancellationToken
-            ).ConfigureAwait(true);
+        (AccountLuck actual, _) = await sut.Hero.Account.GetLuck(
+            accessToken.Key,
+            cancellationToken: TestContext.Current.CancellationToken
+        );
 
-            Assert.True(actual.Luck > 0, "You have no luck today.");
-            Assert.Equal(300, actual.MagicFind);
-            Assert.True(actual.MagicFindWithoutLevelCap >= actual.MagicFind);
-        });
+        Assert.True(actual.Luck > 0, "You have no luck today.");
+        Assert.Equal(300, actual.MagicFind);
+        Assert.True(actual.MagicFindWithoutLevelCap >= actual.MagicFind);
     }
 }
