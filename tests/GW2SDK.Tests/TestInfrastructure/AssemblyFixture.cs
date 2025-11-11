dae25e3aeedfe5d4c7e2df2798b44b7ffa2b6e3c
Steven Liekens <steven.liekens@gmail.com>
2025-11-11 13:51:53 +0000

    Add source generator for listing DTO types

--- tests/GW2SDK.Tests/TestInfrastructure/AssemblyFixture.cs
+++ tests/GW2SDK.Tests/TestInfrastructure/AssemblyFixture.cs
@@ -2,16 +2,7 @@
 
 namespace GuildWars2.Tests.TestInfrastructure;
 
-public class AssemblyFixture
+public partial class AssemblyFixture
 {
     public Assembly Assembly { get; } = Assembly.Load("GW2SDK");
-
-    public IEnumerable<Type> DataTransferObjects =>
-        Assembly.DefinedTypes.Where(type => WithAttribute(type, "DataTransferObjectAttribute"));
-
-    private static bool WithAttribute(MemberInfo type, string attributeName)
-    {
-        return type.GetCustomAttributes()
-            .Any(attribute => attribute.GetType().Name == attributeName);
-    }
 }
