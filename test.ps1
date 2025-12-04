4192841ffebbb99d3f70863ae43f5b0ff053405f
Steven Liekens <steven.liekens@gmail.com>
2025-12-04 13:29:54 +0000

    Use dotnet test in helper scripts

--- test.ps1
+++ test.ps1
@@ -1,10 +1,15 @@
 #!/usr/bin/env pwsh
+
 # Remove results from previous runs
 Remove-Item -ErrorAction Ignore -Recurse artifacts/bin/GuildWars2.Tests/debug_net10.0/TestResults/*
 
+# Ignore exit code 8 (test session ran zero tests)
+$env:TESTINGPLATFORM_EXITCODE_IGNORE = "8"
+
+
 # Run tests with coverage, additional arguments are passed on to dotnet test
 # e.g. ./test.ps1 --treenode-filter */*/Colors*/*
-dotnet run --project tests/GuildWars2.Tests --framework net10.0 -- `
+dotnet test --framework net10.0 `
     --config-file tests/testconfig.json `
     --coverage `
     @args
--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# Ignore exit code 8 (test session ran zero tests)
+export TESTINGPLATFORM_EXITCODE_IGNORE=8
+
 # Remove results from previous runs
 rm -r artifacts/bin/GuildWars2.Tests/debug_net10.0/TestResults/* || true
 
 # Run tests with coverage, additional arguments are passed on to dotnet test
 # e.g. ./test.sh --treenode-filter */*/Colors*/*
-dotnet run --project tests/GuildWars2.Tests --framework net10.0 -- \
+dotnet test --framework net10.0 \
     --config-file tests/testconfig.json \
     --coverage \
     "$@"
