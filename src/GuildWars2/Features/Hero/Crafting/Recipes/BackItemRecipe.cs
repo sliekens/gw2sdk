29c722a4577d1e72b9b2739594022a7220e4164e
Steven Liekens <steven.liekens@gmail.com>
2026-01-07 21:24:34 +0000

    Amend

--- src/GuildWars2/Features/Hero/Crafting/Recipes/BackItemRecipe.cs
+++ src/GuildWars2/Features/Hero/Crafting/Recipes/BackItemRecipe.cs
@@ -2,6 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace GuildWars2.Hero.Crafting.Recipes;
 
-/// <summary>Information about a recipe for crafting a backpack.</summary>
+/// <summary>Information about a recipe for crafting a back item.</summary>
 [JsonConverter(typeof(BackItemRecipeJsonConverter))]
 public sealed record BackItemRecipe : Recipe;
